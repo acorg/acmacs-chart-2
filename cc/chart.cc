d83da1d8c3a955b6ae4e96baecfc7f2f2699473c
Eugene Skepner <github@skepner.eu>
2017-12-19 12:59:08 +0100

    debugging

--- cc/chart.cc
+++ cc/chart.cc
@@ -204,7 +204,7 @@ void acmacs::chart::Chart::serum_coverage(size_t aAntigenNo, size_t aSerumNo, In
 void acmacs::chart::Chart::set_homologous(bool force, SeraP aSera) const
 {
     if (!mHomologousFound && (force || (!is_merge() && number_of_antigens() < 200))) {
-        Timeit ti("set homologous for sera: ");
+        // Timeit ti("set homologous for sera: ");
         if (!aSera)
             aSera = sera();
         aSera->set_homologous(*antigens());
