bb08475e5fce7307be9977899e3269dc0ad0c53e
Eugene Skepner <github@skepner.eu>
2018-01-03 14:04:09 +0100

    stress calculation development

--- cc/chart-stress.cc
+++ cc/chart-stress.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "acmacs-base/argc-argv.hh"
+#include "acmacs-base/string.hh"
 #include "acmacs-chart-2/factory-import.hh"
 #include "acmacs-chart-2/chart.hh"
 
@@ -26,7 +27,9 @@ int main(int argc, char* const argv[])
             const report_time report = args["--time"] ? report_time::Yes : report_time::No;
             auto chart = acmacs::chart::import_from_file(args[0], acmacs::chart::Verify::None, report);
             auto stress = chart->make_stress<double>(args["--projection"]);
-            std::cerr << "stress: " << chart->projection(args["--projection"])->calculate_stress(stress) << '\n';
+            std::cerr << "stress d: " << acmacs::to_string(chart->projection(args["--projection"])->calculate_stress(stress)) << '\n';
+            auto stress_f = chart->make_stress<float>(args["--projection"]);
+            std::cerr << "stress f: " << acmacs::to_string(chart->projection(args["--projection"])->calculate_stress(stress_f)) << '\n';
         }
     }
     catch (std::exception& err) {
