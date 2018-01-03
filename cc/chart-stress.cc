af87a0535e5896e4279080932bf1a4a817fb3d8b
Eugene Skepner <github@skepner.eu>
2018-01-03 16:21:41 +0100

    stress calculation development

--- cc/chart-stress.cc
+++ cc/chart-stress.cc
@@ -2,8 +2,24 @@
 
 #include "acmacs-base/argc-argv.hh"
 #include "acmacs-base/string.hh"
+#include "acmacs-base/timeit.hh"
 #include "acmacs-chart-2/factory-import.hh"
 #include "acmacs-chart-2/chart.hh"
+
+// ----------------------------------------------------------------------
+
+template <typename Float> double measure(acmacs::chart::ProjectionP projection, const acmacs::chart::Stress<Float>& stress)
+{
+    constexpr const double test_duration{10};
+    const auto start = acmacs::timestamp();
+    double duration{0};
+    size_t count = 0;
+    for (; duration < test_duration; duration = acmacs::elapsed(start)) {
+        for (size_t i = 0; i < 1000; ++i, ++count)
+            projection->calculate_stress(stress);
+    }
+    return count / duration;
+}
 
 // ----------------------------------------------------------------------
 
@@ -13,7 +29,7 @@ int main(int argc, char* const argv[])
     try {
         argc_argv args(argc, argv, {
                 {"--projection", 0L},
-                {"--time", false, "report time of loading chart"},
+                {"--time", false, "test speed"},
                 {"--verbose", false},
                 {"-h", false},
                 {"--help", false},
@@ -26,10 +42,17 @@ int main(int argc, char* const argv[])
         else {
             const report_time report = args["--time"] ? report_time::Yes : report_time::No;
             auto chart = acmacs::chart::import_from_file(args[0], acmacs::chart::Verify::None, report);
+            auto projection = chart->projection(args["--projection"]);
             auto stress = chart->make_stress<double>(args["--projection"]);
-            std::cerr << "stress d: " << acmacs::to_string(chart->projection(args["--projection"])->calculate_stress(stress)) << '\n';
+            if (args["--time"])
+                std::cerr << "stress d: " << acmacs::to_string(projection->calculate_stress(stress)) << "   per second: " << measure(projection, stress) << '\n';
+            else
+                std::cerr << "stress d: " << acmacs::to_string(projection->calculate_stress(stress)) << '\n';
             auto stress_f = chart->make_stress<float>(args["--projection"]);
-            std::cerr << "stress f: " << acmacs::to_string(chart->projection(args["--projection"])->calculate_stress(stress_f)) << '\n';
+            if (args["--time"])
+                std::cerr << "stress f: " << acmacs::to_string(projection->calculate_stress(stress_f)) << "   per second: " << measure(projection, stress_f) << '\n';
+            else
+                std::cerr << "stress f: " << acmacs::to_string(projection->calculate_stress(stress_f)) << '\n';
         }
     }
     catch (std::exception& err) {
--- cc/stress.cc
+++ cc/stress.cc
@@ -13,11 +13,11 @@ template <typename Float> Float acmacs::chart::Stress<Float>::value(const std::v
 
     constexpr const Float SigmoidMutiplier{10};
 
-    auto map_distance = [&aArgument,this](const index_t index) {
+    auto map_distance = [&aArgument,num_dim=this->number_of_dimensions_](const index_t& index) {
         return acmacs::vector_math::distance<Float>(
-            aArgument.begin() + static_cast<diff_t>(this->number_of_dimensions_ * index->first),
-            aArgument.begin() + static_cast<diff_t>(this->number_of_dimensions_ * (index->first + 1)),
-            aArgument.begin() + static_cast<diff_t>(this->number_of_dimensions_ * index->second));
+            aArgument.begin() + static_cast<diff_t>(num_dim * index->first),
+            aArgument.begin() + static_cast<diff_t>(num_dim * (index->first + 1)),
+            aArgument.begin() + static_cast<diff_t>(num_dim * index->second));
     };
 
     auto contribution_regular = [map_distance,index=table_distances().regular_indexes().begin()](Float sum, Float table_distance) mutable {
@@ -27,7 +27,7 @@ template <typename Float> Float acmacs::chart::Stress<Float>::value(const std::v
 
     auto contribution_less_than = [map_distance,index=table_distances().less_than_indexes().begin()](Float sum, Float table_distance) mutable {
         const Float diff = table_distance - map_distance(index++) + 1;
-        return sum + diff * diff * sigmoid(diff, SigmoidMutiplier);
+        return sum + diff * diff * sigmoid(diff * SigmoidMutiplier);
     };
 
     return std::accumulate(table_distances().regular_distances().begin(), table_distances().regular_distances().end(), Float{0}, contribution_regular)
