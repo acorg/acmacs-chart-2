9c61823c20a963d7f560f3272756606aaaf47f90
Eugene Skepner <github@skepner.eu>
2018-01-30 13:10:04 +0100

    chart-common development

--- Makefile
+++ Makefile
@@ -25,7 +25,8 @@ TARGETS = \
 SOURCES = chart.cc titers.cc column-bases.cc bounding-ball.cc stress.cc optimize.cc \
     rjson-import.cc \
     factory-import.cc ace-import.cc acd1-import.cc lispmds-import.cc lispmds-token.cc \
-    factory-export.cc ace-export.cc lispmds-export.cc lispmds-encode.cc chart-modify.cc
+    factory-export.cc ace-export.cc lispmds-export.cc lispmds-encode.cc chart-modify.cc \
+    common.cc
 
 ALGLIB = alglib-3.13.0
 ALGLIB_SOURCES = optimization.cpp ap.cpp alglibinternal.cpp linalg.cpp alglibmisc.cpp \
--- cc/common.hh
+++ cc/common.hh
@@ -1,6 +1,30 @@
 #pragma once
 
+#include <memory>
+
 // ----------------------------------------------------------------------
+
+namespace acmacs::chart
+{
+    class Chart;
+
+    class CommonAntigensSera
+    {
+     public:
+        enum class match_level_t { strict, relaxed, ignored, automatic };
+
+        CommonAntigensSera(const Chart& primary, const Chart& secondary, match_level_t match_level);
+        ~CommonAntigensSera();
+
+        void report() const;
+
+     private:
+        class Impl;
+        std::unique_ptr<Impl> impl_;
+
+    }; // class CommonAntigensSera
+
+} // namespace acmacs::chart
 
 /// ----------------------------------------------------------------------
 /// Local Variables:
