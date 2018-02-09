ee350e2be99c95ff2e334e3a3c7e5fc3357d47fe
Eugene Skepner <github@skepner.eu>
2018-02-09 11:09:10 +0100

    html representation of a table

--- cc/chart-html.cc
+++ cc/chart-html.cc
@@ -5,12 +5,13 @@
 #include "acmacs-base/enumerate.hh"
 #include "acmacs-base/range.hh"
 #include "acmacs-base/string.hh"
+#include "acmacs-base/string-split.hh"
 #include "acmacs-base/read-file.hh"
 #include "acmacs-base/quicklook.hh"
 #include "acmacs-chart-2/factory-import.hh"
 #include "acmacs-chart-2/chart.hh"
 
-static void contents(std::ostream& output, const acmacs::chart::Chart& chart);
+static void contents(std::ostream& output, const acmacs::chart::Chart& chart, bool all_fields);
 static void header(std::ostream& output, std::string table_name);
 static void footer(std::ostream& output);
 static std::string html_escape(std::string source);
@@ -24,6 +25,7 @@ int main(int argc, char* const argv[])
     int exit_code = 0;
     try {
         argc_argv args(argc, argv, {
+                {"--all-fields", false},
                 {"--open", false},
                 {"--verbose", false},
                 {"--time", false, "report time of loading chart"},
@@ -43,12 +45,12 @@ int main(int argc, char* const argv[])
 
             auto chart = acmacs::chart::import_from_file(args[0], acmacs::chart::Verify::None, report);
             header(output, chart->make_name());
-            contents(output, *chart);
+            contents(output, *chart, args["--all-fields"]);
             footer(output);
             output.close();
 
             if (args["--open"] || args.number_of_arguments() < 2)
-                acmacs::quicklook(output_filename, 2);
+                acmacs::open(output_filename);
         }
     }
     catch (std::exception& err) {
@@ -60,10 +62,30 @@ int main(int argc, char* const argv[])
 
 // ----------------------------------------------------------------------
 
-void contents(std::ostream& output, const acmacs::chart::Chart& chart)
+struct AntigenFields
+{
+    bool annotations;
+    bool reassortants;
+    bool passages;
+    bool dates;
+    bool lab_ids;
+
+    size_t skip_left() const { return size_t(annotations) + size_t(reassortants) + size_t(passages); }
+    size_t skip_right() const { return size_t(dates) + size_t(lab_ids); }
+};
+
+static void serum_rows(std::ostream& output, const acmacs::chart::Chart& chart, bool all_fields, const AntigenFields& antigen_fields);
+
+inline bool field_present(const std::vector<std::string>& src)
+{
+    return std::find_if(src.begin(), src.end(), [](const auto& s) -> bool { return !s.empty(); }) != src.end();
+};
+
+// ----------------------------------------------------------------------
+
+void contents(std::ostream& output, const acmacs::chart::Chart& chart, bool all_fields)
 {
     output << "<h3>" << chart.make_name() << "</h3>\n";
-    auto field_present = [](const std::vector<std::string>& src) -> bool { return std::find_if(src.begin(), src.end(), [](const auto& s) -> bool { return !s.empty(); }) != src.end(); };
     auto antigens = chart.antigens();
     auto sera = chart.sera();
 
@@ -75,86 +97,21 @@ void contents(std::ostream& output, const acmacs::chart::Chart& chart)
         dates[ag_no] = antigen->date();
         lab_ids[ag_no] = string::join(" ", antigen->lab_ids());
     }
-    std::vector<std::string> serum_annotations(sera->size());
-    for (auto [sr_no, serum]: acmacs::enumerate(*sera)) {
-        serum_annotations[sr_no] = string::join(" ", serum->annotations());
-    }
-    const bool annotations_present{field_present(annotations)}, reassortants_present{field_present(reassortants)}, passages_present{field_present(passages)},
-               dates_present{field_present(dates)}, lab_ids_present{field_present(lab_ids)}, serum_annotations_present{field_present(serum_annotations)};
+    const AntigenFields antigen_fields{field_present(annotations), field_present(reassortants), field_present(passages), field_present(dates), field_present(lab_ids)};
 
-    output << "<table><tr><td></td><td></td>";
-    size_t skip_left = 0;
-    if (annotations_present) {
-        output << "<td></td>";
-        ++skip_left;
-    }
-    if (reassortants_present) {
-        output << "<td></td>";
-        ++skip_left;
-    }
-    if (passages_present) {
-        output << "<td></td>";
-        ++skip_left;
-    }
-    for (auto sr_no : acmacs::range(sera->size()))
-        output << "<td class=\"sr-no " << ("sr-" + std::to_string(sr_no)) << "\">" << (sr_no + 1) << "</td>";
-    size_t skip_right = 0;
-    if (dates_present) {
-        output << "<td></td>";
-        ++skip_right;
-    }
-    if (lab_ids_present) {
-        output << "<td></td>";
-        ++skip_right;
-    }
-    output << "</tr>\n";
-
-      // serum names
-    output << "<tr>";
-    for ([[maybe_unused]] auto i : acmacs::range(skip_left + 2))
-        output << "<td></td>";
-    for (auto [sr_no, serum] : acmacs::enumerate(*sera)) {
-        const auto sr_no_class = "sr-" + std::to_string(sr_no);
-        output << "<td class=\"sr-name " << sr_no_class << "\">" << html_escape(serum->name_abbreviated()) << "</td>";
-    }
-    for ([[maybe_unused]] auto i : acmacs::range(skip_right))
-        output << "<td></td>";
-    output << "</tr>\n";
-
-      // serum annotations (e.g. CONC)
-    if (serum_annotations_present) {
-        output << "<tr>";
-        for ([[maybe_unused]] auto i : acmacs::range(skip_left + 2))
-            output << "<td></td>";
-        for (auto [sr_no, serum] : acmacs::enumerate(*sera))
-            output << "<td class=\"sr-annotations " << ("sr-" + std::to_string(sr_no)) << "\">" << html_escape(serum_annotations[sr_no]) << "</td>";
-        for ([[maybe_unused]] auto i : acmacs::range(skip_right))
-            output << "<td></td>";
-        output << "</tr>\n";
-    }
-
-      // serum_ids
-    output << "<tr>";
-    for ([[maybe_unused]] auto i : acmacs::range(skip_left + 2))
-        output << "<td></td>";
-    for (auto [sr_no, serum] : acmacs::enumerate(*sera)) {
-        const auto sr_no_class = "sr-" + std::to_string(sr_no);
-        output << "<td class=\"sr-id " << sr_no_class << "\">" << html_escape(serum->serum_id()) << "</td>";
-    }
-    for ([[maybe_unused]] auto i : acmacs::range(skip_right))
-        output << "<td></td>";
-    output << "</tr>\n";
+    output << "<table>\n";
+    serum_rows(output, chart, all_fields, antigen_fields);
 
     auto titers = chart.titers();
       // antigens and titers
     for (auto [ag_no, antigen]: acmacs::enumerate(*antigens)) {
         output << "<tr class=\"" << ((ag_no % 2) == 0 ? "even" : "odd") << ' ' << ("ag-" + std::to_string(ag_no)) << "\"><td class=\"ag-no\">" << (ag_no + 1) << "</td>"
                << "<td class=\"ag-name\">" << html_escape(antigen->name()) << "</td>";
-        if (annotations_present)
+        if (antigen_fields.annotations)
             output << "<td class=\"auto ag-annotations\">" << html_escape(annotations[ag_no]) << "</td>";
-        if (reassortants_present)
+        if (antigen_fields.reassortants)
             output << "<td class=\"ag-reassortant\">" << html_escape(reassortants[ag_no]) << "</td>";
-        if (passages_present)
+        if (antigen_fields.passages)
             output << "<td class=\"ag-passage\">" << html_escape(passages[ag_no]) << "</td>";
 
           // titers
@@ -174,9 +131,9 @@ void contents(std::ostream& output, const acmacs::chart::Chart& chart)
             output << "<td class=\"titer " << titer_class << ' ' << titer_pos_class << ' ' << sr_no_class << "\">" << html_escape(acmacs::to_string(titer)) << "</td>";
         }
 
-        if (dates_present)
+        if (antigen_fields.dates)
             output << "<td class=\"ag-date\">" << dates[ag_no] << "</td>";
-        if (lab_ids_present)
+        if (antigen_fields.lab_ids)
             output << "<td class=\"ag-lab-id\">" << lab_ids[ag_no] << "</td>";
         output << "</tr>\n";
     }
@@ -195,6 +152,75 @@ void contents(std::ostream& output, const acmacs::chart::Chart& chart)
 
 // ----------------------------------------------------------------------
 
+void serum_rows(std::ostream& output, const acmacs::chart::Chart& chart, bool all_fields, const AntigenFields& antigen_fields)
+{
+    auto sera = chart.sera();
+    auto make_skip = [&output] (size_t count) { output << "<td colspan=\"" + std::to_string(count) + "\"></td>"; };
+
+      // serum no
+    output << "<tr>\n";
+    make_skip(antigen_fields.skip_left() + 2);
+    for (auto sr_no : acmacs::range(sera->size()))
+        output << "<td class=\"sr-no " << ("sr-" + std::to_string(sr_no)) << "\">" << (sr_no + 1) << "</td>";
+    make_skip(antigen_fields.skip_right());
+    output << "</tr>\n";
+
+    if (all_fields) {
+          // serum names
+        output << "<tr>\n";
+        make_skip(antigen_fields.skip_left() + 2);
+        for (auto [sr_no, serum] : acmacs::enumerate(*sera)) {
+            output << "<td class=\"sr-name sr-" << sr_no << "\">" << html_escape(serum->name_abbreviated()) << "</td>";
+        }
+        make_skip(antigen_fields.skip_right());
+        output << "</tr>\n";
+
+          // serum annotations (e.g. CONC)
+        std::vector<std::string> serum_annotations(sera->size());
+        for (auto [sr_no, serum]: acmacs::enumerate(*sera)) {
+            serum_annotations[sr_no] = string::join(" ", serum->annotations());
+        }
+        if (field_present(serum_annotations)) {
+            output << "<tr>";
+            make_skip(antigen_fields.skip_left() + 2);
+            for (auto [sr_no, serum] : acmacs::enumerate(*sera))
+                output << "<td class=\"sr-annotations " << ("sr-" + std::to_string(sr_no)) << "\">" << html_escape(serum_annotations[sr_no]) << "</td>";
+            make_skip(antigen_fields.skip_right());
+            output << "</tr>\n";
+        }
+
+          // serum_ids
+        output << "<tr>";
+        make_skip(antigen_fields.skip_left() + 2);
+        for (auto [sr_no, serum] : acmacs::enumerate(*sera)) {
+            const auto sr_no_class = "sr-" + std::to_string(sr_no);
+            output << "<td class=\"sr-id " << sr_no_class << "\">" << html_escape(serum->serum_id()) << "</td>";
+        }
+        make_skip(antigen_fields.skip_right());
+        output << "</tr>\n";
+    }
+    else {
+        output << "<tr>\n";
+        make_skip(antigen_fields.skip_left() + 2);
+        for (auto [sr_no, serum] : acmacs::enumerate(*sera)) {
+            std::string name = serum->name_abbreviated();
+            if (name.size() > 8) {
+                std::vector<std::string> fields;
+                for (auto field : acmacs::string::split(name, "/", acmacs::string::Split::RemoveEmpty))
+                    fields.emplace_back(field.substr(0, 2));
+                name = string::join("/", fields);
+            }
+            output << "<td class=\"sr-name sr-" << sr_no << "\"><div class=\"tooltip\">" << html_escape(name)
+                   << "<span class=\"tooltiptext\">" << html_escape(serum->full_name()) << "</span></div></td>";
+        }
+        make_skip(antigen_fields.skip_right());
+        output << "</tr>\n";
+    }
+
+} // sera
+
+// ----------------------------------------------------------------------
+
 void header(std::ostream& output, std::string table_name)
 {
     const char* h1 = R"(<!DOCTYPE html>
@@ -206,6 +232,7 @@ table { border-collapse: collapse; border: 1px solid grey; border-spacing: 0; }
 table.td { border: 1px solid grey; }
 .sr-no { text-align: center; font-size: 0.8em; }
 .sr-name { text-align: center; padding: 0 0.5em; white-space: nowrap; font-weight: bold; font-size: 0.8em; }
+.sr-name .tooltip .tooltiptext { top: -3em; left: -10em; }
 .sr-id { text-align: center; white-space: nowrap; font-size: 0.5em; border-bottom: 1px solid grey; }
 .sr-annotations { text-align: center; white-space: nowrap; font-size: 0.5em; }
 .ag-no { text-align: right; font-size: 0.8em; }
@@ -226,6 +253,30 @@ table.td { border: 1px solid grey; }
 .titer-dodgy     { color: brown; }
 tr.even td { background-color: white; }
 tr.odd td { background-color: #F0F0F0; }
+
+.tooltip {
+    position: relative;
+    display: inline-block;
+    border-bottom: 1px dotted black;
+}
+
+.tooltip .tooltiptext {
+    visibility: hidden;
+    background-color: #EEF;
+    color: black;
+    text-align: left;
+    padding: 5px 3px;
+    border-radius: 6px;
+    border: 1px solid grey;
+    position: absolute;
+    top: 1em;
+    left: 0;
+    z-index: 1;
+}
+
+.tooltip:hover .tooltiptext {
+    visibility: visible;
+}
 </style>
 <title>)";
     const char* h2 = R"(</title>
