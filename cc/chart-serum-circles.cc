f4e8689e5d1334b738cb96b9591632e9c42ba470
Eugene Skepner <github@skepner.eu>
2018-02-22 17:13:30 +0100

    chart serum circles report

--- cc/chart-serum-circles.cc
+++ cc/chart-serum-circles.cc
@@ -49,8 +49,8 @@ struct AntigenData
     size_t antigen_no;
     acmacs::chart::AntigenP antigen;
     acmacs::chart::Titer titer;
-    double theoretical;
-    double empirical;
+    double theoretical = -1;
+    double empirical = -1;
 };
 
 struct SerumData
@@ -64,6 +64,7 @@ struct SerumData
 };
 
 static std::vector<SerumData> collect(const acmacs::chart::Chart& chart, size_t projection_no);
+static void report_text(const acmacs::chart::Chart& chart, const std::vector<SerumData>& sera_data);
 
 void report(const argc_argv& args)
 {
@@ -75,37 +76,38 @@ void report(const argc_argv& args)
         throw std::runtime_error("invalid projection number");
     chart->set_homologous(acmacs::chart::Chart::find_homologous_for_big_chart::yes);
     auto result = collect(*chart, projection_no);
+    report_text(*chart, result);
 
-    auto antigens = chart->antigens();
-    const auto antigen_no_num_digits = static_cast<int>(std::log10(antigens->size())) + 1;
-    auto sera = chart->sera();
-    const auto serum_no_num_digits = static_cast<int>(std::log10(sera->size())) + 1;
-    auto titers = chart->titers();
-    for (auto [sr_no, serum]: acmacs::enumerate(*sera)) {
-        const auto antigen_indexes = serum->homologous_antigens();
+    // auto antigens = chart->antigens();
+    // const auto antigen_no_num_digits = static_cast<int>(std::log10(antigens->size())) + 1;
+    // auto sera = chart->sera();
+    // const auto serum_no_num_digits = static_cast<int>(std::log10(sera->size())) + 1;
+    // auto titers = chart->titers();
+    // for (auto [sr_no, serum]: acmacs::enumerate(*sera)) {
+    //     const auto antigen_indexes = serum->homologous_antigens();
 
-        std::cout << "SR " << std::setw(serum_no_num_digits) << sr_no << ' ' << serum->full_name_with_fields() << '\n';
-        if (!antigen_indexes.empty()) {
-            std::cout << "   titer theor empir\n";
-            for (auto ag_no : antigen_indexes) {
-                std::cout << "  ";
-                if (const auto homologous_titer = titers->titer(ag_no, sr_no); homologous_titer.is_regular()) {
-                    const auto theoretical = chart->serum_circle_radius_theoretical(ag_no, sr_no, projection_no, false);
-                    const auto empirical = chart->serum_circle_radius_empirical(ag_no, sr_no, projection_no, false);
-                    std::cout << std::setw(6) << std::right << homologous_titer
-                              << ' ' << std::setw(5) << std::fixed << std::setprecision(2) << theoretical
-                              << ' ' << std::setw(5) << std::fixed << std::setprecision(2) << empirical;
-                }
-                else {
-                    std::cout << std::setw(5) << std::right << homologous_titer << "    -     -  ";
-                }
-                std::cout << "   " << std::setw(antigen_no_num_digits) << ag_no << ' ' << (*antigens)[ag_no]->full_name_with_passage() << '\n';
-            }
-        }
-        else {
-            std::cout << "    no antigens\n";
-        }
-    }
+    //     std::cout << "SR " << std::setw(serum_no_num_digits) << sr_no << ' ' << serum->full_name_with_fields() << '\n';
+    //     if (!antigen_indexes.empty()) {
+    //         std::cout << "   titer theor empir\n";
+    //         for (auto ag_no : antigen_indexes) {
+    //             std::cout << "  ";
+    //             if (const auto homologous_titer = titers->titer(ag_no, sr_no); homologous_titer.is_regular()) {
+    //                 const auto theoretical = chart->serum_circle_radius_theoretical(ag_no, sr_no, projection_no, false);
+    //                 const auto empirical = chart->serum_circle_radius_empirical(ag_no, sr_no, projection_no, false);
+    //                 std::cout << std::setw(6) << std::right << homologous_titer
+    //                           << ' ' << std::setw(5) << std::fixed << std::setprecision(2) << theoretical
+    //                           << ' ' << std::setw(5) << std::fixed << std::setprecision(2) << empirical;
+    //             }
+    //             else {
+    //                 std::cout << std::setw(5) << std::right << homologous_titer << "    -     -  ";
+    //             }
+    //             std::cout << "   " << std::setw(antigen_no_num_digits) << ag_no << ' ' << (*antigens)[ag_no]->full_name_with_passage() << '\n';
+    //         }
+    //     }
+    //     else {
+    //         std::cout << "    no antigens\n";
+    //     }
+    // }
 
 } // report
 
@@ -130,6 +132,36 @@ std::vector<SerumData> collect(const acmacs::chart::Chart& chart, size_t project
     return result;
 
 } // collect
+
+// ----------------------------------------------------------------------
+
+void report_text(const acmacs::chart::Chart& chart, const std::vector<SerumData>& sera_data)
+{
+    const auto antigen_no_num_digits = static_cast<int>(std::log10(chart.number_of_antigens())) + 1;
+    const auto serum_no_num_digits = static_cast<int>(std::log10(chart.number_of_sera())) + 1;
+    for (const auto& serum_data : sera_data) {
+        std::cout << "SR " << std::setw(serum_no_num_digits) << serum_data.serum_no << ' ' << serum_data.serum->full_name_with_fields() << '\n';
+        if (!serum_data.antigens.empty()) {
+            std::cout << "   titer theor empir\n";
+            for (const auto& antigen_data : serum_data.antigens) {
+                std::cout << "  " << std::setw(6) << std::right << antigen_data.titer;
+                if (antigen_data.theoretical > 0)
+                    std::cout << ' ' << std::setw(5) << std::fixed << std::setprecision(2) << antigen_data.theoretical;
+                else
+                    std::cout << std::setw(6) << ' ';
+                if (antigen_data.empirical > 0)
+                    std::cout << ' ' << std::setw(5) << std::fixed << std::setprecision(2) << antigen_data.empirical;
+                else
+                    std::cout << std::setw(6) << ' ';
+                std::cout << "   " << std::setw(antigen_no_num_digits) << antigen_data.antigen_no << ' ' << antigen_data.antigen->full_name_with_passage() << '\n';
+            }
+        }
+        else {
+            std::cout << "    no antigens\n";
+        }
+    }
+
+} // report_text
 
 // ----------------------------------------------------------------------
 
