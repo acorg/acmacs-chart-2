ab16adbd5c5830f491a4c4218626f6b2c4e3449d
Eugene Skepner <github@skepner.eu>
2018-02-22 16:35:58 +0100

    antigen/serum names with fields report

--- cc/chart-names.cc
+++ cc/chart-names.cc
@@ -15,6 +15,7 @@ int main(int argc, char* const argv[])
     int exit_code = 0;
     try {
         argc_argv args(argc, argv, {
+                {"--fields", false, "report names with fields"},
                 {"--verbose", false},
                 {"--time", false, "report time of loading chart"},
                 {"-h", false},
@@ -26,17 +27,27 @@ int main(int argc, char* const argv[])
             exit_code = 1;
         }
         else {
-            const report_time report = args["--time"] ? report_time::Yes : report_time::No;
+            const auto report = do_report_time(args["--time"]);
+            const bool fields = args["--fields"];
             for (size_t file_no = 0; file_no < args.number_of_arguments(); ++file_no) {
                 auto chart = acmacs::chart::import_from_file(args[file_no], acmacs::chart::Verify::None, report);
                 auto antigens = chart->antigens();
                 auto sera = chart->sera();
                 const auto num_digits = static_cast<int>(std::log10(std::max(antigens->size(), sera->size()))) + 1;
                 for (auto [ag_no, antigen]: acmacs::enumerate(*antigens)) {
-                    std::cout << "AG " << std::setw(num_digits) << ag_no << " " << string::join(" ", {antigen->name(), string::join(" ", antigen->annotations()), antigen->reassortant(), antigen->passage(), "[" + static_cast<std::string>(antigen->date()) + "]", string::join(" ", antigen->lab_ids())}) << (antigen->reference() ? " Ref" : "") << '\n';
+                    std::cout << "AG " << std::setw(num_digits) << ag_no << ' ';
+                    if (fields)
+                        std::cout << antigen->full_name_with_fields() << '\n';
+                    else
+                        std::cout << string::join(" ", {antigen->full_name_with_passage(), "[" + static_cast<std::string>(antigen->date()) + "]", string::join(" ", antigen->lab_ids())})
+                                  << (antigen->reference() ? " Ref" : "") << '\n';
                 }
                 for (auto [sr_no, serum]: acmacs::enumerate(*sera)) {
-                    std::cout << "SR " << std::setw(num_digits) << sr_no << " " << string::join(" ", {serum->name(), string::join(" ", serum->annotations()), serum->reassortant(), serum->passage(), serum->serum_id(), serum->serum_species()}) << '\n';
+                    std::cout << "SR " << std::setw(num_digits) << sr_no << ' ';
+                    if (fields)
+                        std::cout << serum->full_name_with_fields() << '\n';
+                    else
+                        std::cout << string::join(" ", {serum->full_name_with_passage(), serum->serum_species()}) << '\n';
                 }
             }
         }
