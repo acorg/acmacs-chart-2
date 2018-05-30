f1bf28ce19e3d75e76923410313482bfe1b2c722
Eugene Skepner <github@skepner.eu>
2018-05-30 17:39:53 +0200

    bug fix

--- cc/ace-export.hh
+++ cc/ace-export.hh
@@ -12,8 +12,8 @@ namespace acmacs::chart
     std::string export_ace(const Chart& aChart, std::string aProgramName, size_t aIndent);
 
     template <typename DF> std::string export_layout(const Chart& aChart, size_t aProjectionNo = 0);
-    extern template std::string acmacs::chart::export_layout<acmacs::DataFormatterSpaceSeparated>(const Chart& aChart, size_t aProjectionNo);
-    extern template std::string acmacs::chart::export_layout<acmacs::DataFormatterCSV>(const Chart& aChart, size_t aProjectionNo);
+    extern template std::string export_layout<acmacs::DataFormatterSpaceSeparated>(const Chart& aChart, size_t aProjectionNo);
+    extern template std::string export_layout<acmacs::DataFormatterCSV>(const Chart& aChart, size_t aProjectionNo);
 
 } // namespace acmacs::chart
 
