ab1149f867844772a25e6a361e8fd6d0a9e4ec92
Eugene Skepner <github@skepner.eu>
2018-07-19 11:40:15 +0200

    minor improvement

--- cc/factory-import.hh
+++ cc/factory-import.hh
@@ -13,7 +13,7 @@ namespace acmacs::chart
     class Chart;
     using ChartP = std::shared_ptr<Chart>;
 
-    ChartP import_from_file(std::string aFilename, Verify aVerify, report_time aReport);
+    ChartP import_from_file(std::string aFilename, Verify aVerify = Verify::None, report_time aReport = report_time::No);
     ChartP import_from_data(std::string aData, Verify aVerify, report_time aReport);
     ChartP import_from_data(std::string_view aData, Verify aVerify, report_time aReport);
     ChartP import_from_decompressed_data(std::string aData, Verify aVerify, report_time aReport);
