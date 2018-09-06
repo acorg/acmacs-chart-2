4aa6f7ae326acfce0eb674c06093860c47d61df1
Eugene Skepner <github@skepner.eu>
2018-09-06 18:11:51 +0200

    chart-transformation

---
+++ cc/chart-transformation.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+
+#include "acmacs-base/argc-argv.hh"
+#include "acmacs-base/string.hh"
+#include "acmacs-base/timeit.hh"
+#include "acmacs-chart-2/factory-import.hh"
+#include "acmacs-chart-2/chart.hh"
+
+// ----------------------------------------------------------------------
+
+int main(int argc, char* const argv[])
+{
+    int exit_code = 0;
+    try {
+        argc_argv args(argc, argv, {
+                {"--projection", 0L},
+                {"--precision", 5U, "report precision (double)"},
+                {"--time", false, "test speed"},
+                {"-h", false},
+                {"--help", false},
+                {"-v", false},
+                        });
+        if (args["-h"] || args["--help"] || args.number_of_arguments() != 1) {
+            std::cerr << "Usage: " << args.program() << " [options] <chart-file>\n" << args.usage_options() << '\n';
+            exit_code = 1;
+        }
+        else {
+            const auto report = do_report_time(args["--time"]);
+            const int precision = args["--precision"];
+            auto chart = acmacs::chart::import_from_file(args[0], acmacs::chart::Verify::None, report);
+            auto projection = chart->projection(args["--projection"]);
+            const auto transformation = projection->transformation();
+            std::cout << std::setprecision(precision) << transformation.a << ' ' << transformation.b << ' ' << transformation.c << ' ' << transformation.d << '\n';
+        }
+    }
+    catch (std::exception& err) {
+        std::cerr << "ERROR: " << err.what() << '\n';
+        exit_code = 2;
+    }
+    return exit_code;
+}
+
+// ----------------------------------------------------------------------
+/// Local Variables:
+/// eval: (if (fboundp 'eu-rename-buffer) (eu-rename-buffer))
+/// End:
