7f38878d53abb80c175dbfa9b7bc5609e092fefe
Eugene Skepner <github@skepner.eu>
2018-11-07 12:28:27 +0100

    chart merge report development

--- cc/titers.cc
+++ cc/titers.cc
@@ -343,21 +343,20 @@ std::pair<acmacs::chart::PointIndexList, acmacs::chart::PointIndexList> acmacs::
 
 std::pair<acmacs::chart::PointIndexList, acmacs::chart::PointIndexList> acmacs::chart::Titers::antigens_sera_in_multiple_layers() const
 {
-    std::map<size_t, size_t> antigen_to_num_layers, serum_to_num_layers;
+    std::map<size_t, std::set<size_t>> antigen_to_layers, serum_to_layers;
     for (size_t layer_no = 0; layer_no < number_of_layers(); ++layer_no) {
         for (auto titer_it = begin(layer_no); titer_it != end(layer_no); ++titer_it) {
-            ++antigen_to_num_layers[titer_it->antigen];
-            ++serum_to_num_layers[titer_it->serum];
+            antigen_to_layers[titer_it->antigen].insert(layer_no);
+            serum_to_layers[titer_it->serum].insert(layer_no);
         }
     }
-    std::cerr << "DEBUG: " << antigen_to_num_layers << DEBUG_LINE_FUNC << '\n';
     acmacs::chart::PointIndexList antigens, sera;
-    for (const auto& ag : antigen_to_num_layers) {
-        if (ag.second > 1)
+    for (const auto& ag : antigen_to_layers) {
+        if (ag.second.size() > 1)
             antigens.insert(ag.first);
     }
-    for (const auto& sr : serum_to_num_layers) {
-        if (sr.second > 1)
+    for (const auto& sr : serum_to_layers) {
+        if (sr.second.size() > 1)
             sera.insert(sr.first);
     }
     return {antigens, sera};
