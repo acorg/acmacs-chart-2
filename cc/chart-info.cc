ee31dbcbf50a01f484e60c893582708978aad31b
Eugene Skepner <git@skepner.eu>
2019-01-04 16:25:19 +0100

    argv

--- cc/chart-info.cc
+++ cc/chart-info.cc
@@ -1,52 +1,49 @@
 #include <iostream>
 
-#include "acmacs-base/argc-argv.hh"
+#include "acmacs-base/argv.hh"
 #include "acmacs-base/range.hh"
 #include "acmacs-chart-2/factory-import.hh"
 #include "acmacs-chart-2/chart.hh"
 
 // ----------------------------------------------------------------------
 
+using namespace acmacs::argv;
+struct Options : public argv
+{
+    Options(int a_argc, const char* const a_argv[], on_error on_err = on_error::exit) : argv() { parse(a_argc, a_argv, on_err); }
+
+    option<bool> column_bases{*this, "column-bases"};
+    option<bool> list_tables{*this, "list-tables"};
+    option<bool> verify{*this, "verify"};
+    option<bool> report_time{*this, "time", desc{"report time of loading chart"}};
+
+    argument<str_array> charts{*this, arg_name{"chart-file"}, mandatory};
+};
+
 int main(int argc, char* const argv[])
 {
     int exit_code = 0;
     try {
-        argc_argv args(argc, argv, {
-                {"--column-bases", false},
-                {"--list-tables", false},
-                {"--verify", false},
-                {"--time", false, "report time of loading chart"},
-                {"-h", false},
-                {"--help", false},
-                {"-v", false},
-                {"--verbose", false}
-        });
-        if (args["-h"] || args["--help"] || args.number_of_arguments() < 1) {
-            std::cerr << "Usage: " << args.program() << " [options] <chart-file> ...\n" << args.usage_options() << '\n';
-            exit_code = 1;
-        }
-        else {
-            const bool verify = args["--verify"];
-            const auto report = do_report_time(args["--time"]);
-            for (size_t file_no = 0; file_no < args.number_of_arguments(); ++file_no) {
-                auto chart = acmacs::chart::import_from_file(args[file_no], verify ? acmacs::chart::Verify::All : acmacs::chart::Verify::None, report);
-                std::cout << chart->make_info() << '\n';
-                if (const auto having_too_few_numeric_titers = chart->titers()->having_too_few_numeric_titers(); !having_too_few_numeric_titers.empty())
-                    std::cout << "Points having too few numeric titers: " << having_too_few_numeric_titers.size() << ' ' << having_too_few_numeric_titers << '\n';
-                if (args["--column-bases"]) {
-                    // Timeit ti("column bases computing ");
-                    auto cb = chart->computed_column_bases(acmacs::chart::MinimumColumnBasis{});
-                    std::cout << "computed column bases: " << *cb << '\n';
-                    for (auto projection_no : acmacs::range(chart->number_of_projections())) {
-                        if (auto fcb = chart->projection(projection_no)->forced_column_bases(); fcb)
-                            std::cout << "forced column bases for projection " << projection_no << ": " << *fcb << '\n';
-                    }
+        Options opt(argc, argv);
+        const auto report = do_report_time(opt.report_time);
+        for (size_t file_no = 0; file_no < opt.charts->size(); ++file_no) {
+            auto chart = acmacs::chart::import_from_file((*opt.charts)[file_no], opt.verify ? acmacs::chart::Verify::All : acmacs::chart::Verify::None, report);
+            std::cout << chart->make_info() << '\n';
+            if (const auto having_too_few_numeric_titers = chart->titers()->having_too_few_numeric_titers(); !having_too_few_numeric_titers.empty())
+                std::cout << "Points having too few numeric titers: " << having_too_few_numeric_titers.size() << ' ' << having_too_few_numeric_titers << '\n';
+            if (opt.column_bases) {
+                // Timeit ti("column bases computing ");
+                auto cb = chart->computed_column_bases(acmacs::chart::MinimumColumnBasis{});
+                std::cout << "computed column bases: " << *cb << '\n';
+                for (auto projection_no : acmacs::range(chart->number_of_projections())) {
+                    if (auto fcb = chart->projection(projection_no)->forced_column_bases(); fcb)
+                        std::cout << "forced column bases for projection " << projection_no << ": " << *fcb << '\n';
                 }
-                if (auto info = chart->info(); args["--list-tables"] && info->number_of_sources() > 0) {
-                    std::cout << "\nTables:\n";
-                    for (auto src_no : acmacs::range(info->number_of_sources()))
-                        std::cout << std::setw(3) << src_no << ' ' << info->source(src_no)->make_name() << '\n';
-                }
+            }
+            if (auto info = chart->info(); opt.list_tables && info->number_of_sources() > 0) {
+                std::cout << "\nTables:\n";
+                for (auto src_no : acmacs::range(info->number_of_sources()))
+                    std::cout << std::setw(3) << src_no << ' ' << info->source(src_no)->make_name() << '\n';
             }
         }
     }
--- cc/chart-reorient.cc
+++ cc/chart-reorient.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#include "acmacs-base/argc-argv.hh"
+#include "acmacs-base/argv.hh"
 #include "acmacs-base/range.hh"
 #include "acmacs-base/filesystem.hh"
 #include "acmacs-chart-2/factory-import.hh"
@@ -11,47 +11,48 @@
 
 // ----------------------------------------------------------------------
 
+using namespace acmacs::argv;
+struct Options : public argv
+{
+    Options(int a_argc, const char* const a_argv[], on_error on_err = on_error::exit) : argv() { parse(a_argc, a_argv, on_err); }
+
+    option<str>  match{*this, "match", dflt{"auto"}, desc{"match level: \"strict\", \"relaxed\", \"ignored\", \"auto\""}};
+    option<int>  master_projection_no{*this, 'm', "master-projection", dflt{0}, desc{"master projection no"}};
+    option<int>  chart_projection_no{*this, 'p', "chart-projection", dflt{0}, desc{"chart projection no, -1 to reorient all"}};
+    option<bool> report_time{*this, "time", desc{"report time of loading chart"}};
+
+    argument<str> master_chart{*this, arg_name{"master-chart"}, mandatory};
+    argument<str> source{*this, arg_name{"chart-to-reorient"}, mandatory};
+    argument<str> output{*this, arg_name{"output-chart"}, mandatory};
+};
+
 int main(int argc, char* const argv[])
 {
     int exit_code = 0;
     try {
-        argc_argv args(argc, argv, {
-                {"--match", "auto", "match level: \"strict\", \"relaxed\", \"ignored\", \"auto\""},
-                {"-m", 0, "master projection no"},
-                {"-p", 0, "chart projection no, -1 to orient all"},
-                {"--time", false, "test speed"},
-                {"--verbose", false},
-                {"-h", false},
-                {"--help", false},
-                {"-v", false},
-                        });
-        if (args["-h"] || args["--help"] || args.number_of_arguments() != 3) {
-            std::cerr << "Usage: " << args.program() << " [options] <master-chart> <chart-to-reorient> <output-chart>\n" << args.usage_options() << '\n';
-            exit_code = 1;
+        Options opt(argc, argv);
+        const auto report = do_report_time(opt.report_time);
+
+        const auto match_level = acmacs::chart::CommonAntigensSera::match_level(opt.match);
+        auto master = acmacs::chart::import_from_file(opt.master_chart, acmacs::chart::Verify::None, report);
+        acmacs::chart::ChartModify to_reorient{acmacs::chart::import_from_file(opt.source, acmacs::chart::Verify::None, report)};
+        acmacs::chart::CommonAntigensSera common(*master, to_reorient, match_level);
+        if (common) {
+            std::cout << "common antigens: " << common.common_antigens() << " sera: " << common.common_sera() << '\n';
+            // std::cout << "common points:" << common.points() << '\n';
+            // common.report();
+            const auto projections = *opt.chart_projection_no < 0 ? acmacs::filled_with_indexes(to_reorient.number_of_projections()) : std::vector<size_t>{static_cast<size_t>(*opt.chart_projection_no)};
+            for (auto projection_no : projections) {
+                auto procrustes_data = acmacs::chart::procrustes(*master->projection(static_cast<size_t>(*opt.master_projection_no)), *to_reorient.projection(projection_no), common.points(), acmacs::chart::procrustes_scaling_t::no);
+                to_reorient.projection_modify(projection_no)->transformation(procrustes_data.transformation);
+                std::cout << "projection: " << projection_no << '\n';
+                std::cout << "transformation: " << acmacs::to_string(procrustes_data.transformation) << '\n';
+                std::cout << "rms: " << acmacs::to_string(procrustes_data.rms) << "\n\n";
+            }
+            acmacs::chart::export_factory(to_reorient, opt.output, fs::path(opt.program_name()).filename(), report);
         }
         else {
-            const auto report = do_report_time(args["--time"]);
-            const auto match_level = acmacs::chart::CommonAntigensSera::match_level(args["--match"]);
-            auto master = acmacs::chart::import_from_file(args[0], acmacs::chart::Verify::None, report);
-            acmacs::chart::ChartModify to_reorient{acmacs::chart::import_from_file(args[1], acmacs::chart::Verify::None, report)};
-            acmacs::chart::CommonAntigensSera common(*master, to_reorient, match_level);
-            if (common) {
-                std::cout << "common antigens: " << common.common_antigens() << " sera: " << common.common_sera() << '\n';
-                  // std::cout << "common points:" << common.points() << '\n';
-                  // common.report();
-                const auto projections = static_cast<int>(args["-p"]) == -1 ? acmacs::filled_with_indexes(to_reorient.number_of_projections()) : std::vector<size_t>{static_cast<size_t>(args["-p"])};
-                for (auto projection_no : projections) {
-                    auto procrustes_data = acmacs::chart::procrustes(*master->projection(args["-m"]), *to_reorient.projection(projection_no), common.points(), acmacs::chart::procrustes_scaling_t::no);
-                    to_reorient.projection_modify(projection_no)->transformation(procrustes_data.transformation);
-                    std::cout << "projection: " << projection_no << '\n';
-                    std::cout << "transformation: " << acmacs::to_string(procrustes_data.transformation) << '\n';
-                    std::cout << "rms: " << acmacs::to_string(procrustes_data.rms) << "\n\n";
-                }
-                acmacs::chart::export_factory(to_reorient, args[2], fs::path(args.program()).filename(), report);
-            }
-            else {
-                std::cerr << "ERROR: no common antigens/sera\n";
-            }
+            std::cerr << "ERROR: no common antigens/sera\n";
         }
     }
     catch (std::exception& err) {
