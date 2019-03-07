f374e73a375140333e4ce7569b7b6b7bbfea8b3c
Eugene Skepner <git@skepner.eu>
2019-03-07 13:52:46 +0100

    minor improvement

--- cc/chart-remove-antigens-sera.cc
+++ cc/chart-remove-antigens-sera.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#include "acmacs-base/argc-argv.hh"
+#include "acmacs-base/argv.hh"
 #include "acmacs-base/string.hh"
 #include "acmacs-base/string-split.hh"
 #include "acmacs-base/filesystem.hh"
@@ -10,44 +10,43 @@
 
 // ----------------------------------------------------------------------
 
+using namespace acmacs::argv;
+struct Options : public argv
+{
+    Options(int a_argc, const char* const a_argv[], on_error on_err = on_error::exit) : argv() { parse(a_argc, a_argv, on_err); }
+
+    option<str>  antigens_to_remove{*this, 'a', "--antigens", desc{"comma separated list of antigen indexes to remove (0-based)"}};
+    option<str>  sera_to_remove{*this, 's', "--sera", desc{"comma separated list of serum indexes to remove (0-based)"}};
+    option<bool> remove_egg{*this, "remove-egg", desc{"remove egg antigens and sera"}};
+    option<bool> remove_projections{*this, "remove-projections"};
+
+    argument<str> source{*this, arg_name{"source-chart"}, mandatory};
+    argument<str> output{*this, arg_name{"output-chart"}, mandatory};
+};
+
 int main(int argc, char* const argv[])
 {
     int exit_code = 0;
     try {
-        argc_argv args(argc, argv, {
-                {"-a", "", "comma separated list of antigen indexes to remove (0-based)"},
-                {"-s", "", "comma separated list of serum indexes to remove (zero based)"},
-                {"--remove-egg", false, "remove egg antigens and sera"},
-                {"--time", false, "report time of loading chart"},
-                {"--verbose", false},
-                {"-h", false},
-                {"--help", false},
-                {"-v", false},
-                        });
-        if (args["-h"] || args["--help"] || args.number_of_arguments() != 2) {
-            std::cerr << "Usage: " << args.program() << " [options] <chart-file> <output-chart-file>\n" << args.usage_options() << '\n';
-            exit_code = 1;
+        Options opt(argc, argv);
+        acmacs::ReverseSortedIndexes antigens_to_remove{opt.antigens_to_remove->empty() ? acmacs::Indexes{} : acmacs::string::split_into_uint(*opt.antigens_to_remove, ",")};
+        acmacs::ReverseSortedIndexes sera_to_remove{opt.sera_to_remove->empty() ? acmacs::Indexes{} : acmacs::string::split_into_uint(*opt.sera_to_remove, ",")};
+        acmacs::chart::ChartModify chart{acmacs::chart::import_from_file(*opt.source, acmacs::chart::Verify::None)};
+        if (opt.remove_egg) {
+            auto ag_egg = chart.antigens()->all_indexes();
+            chart.antigens()->filter_egg(ag_egg);
+            antigens_to_remove.add(ag_egg.data());
+            auto sr_egg = chart.sera()->all_indexes();
+            chart.sera()->filter_egg(sr_egg);
+            sera_to_remove.add(sr_egg.data());
         }
-        else {
-            const std::string antigens(args["-a"]), sera(args["-s"]);
-            acmacs::ReverseSortedIndexes antigens_to_remove{antigens.empty() ? acmacs::Indexes{} : acmacs::string::split_into_uint(antigens, ",")};
-            acmacs::ReverseSortedIndexes sera_to_remove{sera.empty() ? acmacs::Indexes{} : acmacs::string::split_into_uint(sera, ",")};
-            const auto report = do_report_time(args["--time"]);
-            acmacs::chart::ChartModify chart{acmacs::chart::import_from_file(args[0], acmacs::chart::Verify::None, report)};
-            if (args["--remove-egg"]) {
-                auto ag_egg = chart.antigens()->all_indexes();
-                chart.antigens()->filter_egg(ag_egg);
-                antigens_to_remove.add(ag_egg.data());
-                auto sr_egg = chart.sera()->all_indexes();
-                chart.sera()->filter_egg(sr_egg);
-                sera_to_remove.add(sr_egg.data());
-            }
-            if (!antigens_to_remove.empty())
-                chart.remove_antigens(antigens_to_remove);
-            if (!sera_to_remove.empty())
-                chart.remove_sera(sera_to_remove);
-            acmacs::chart::export_factory(chart, args[1], fs::path(args.program()).filename(), report);
-        }
+        if (opt.remove_projections)
+            chart.projections_modify()->remove_all();
+        if (!antigens_to_remove.empty())
+            chart.remove_antigens(antigens_to_remove);
+        if (!sera_to_remove.empty())
+            chart.remove_sera(sera_to_remove);
+        acmacs::chart::export_factory(chart, *opt.output, fs::path(opt.program_name()).filename());
     }
     catch (std::exception& err) {
         std::cerr << "ERROR: " << err.what() << '\n';
--- cc/factory-export.hh
+++ cc/factory-export.hh
@@ -12,11 +12,11 @@ namespace acmacs::chart
 {
     class Chart;
 
-    void export_factory(const Chart& aChart, std::string aFilename, std::string aProgramName, report_time aReport);
-    inline void export_factory(const Chart& aChart, std::string_view aFilename, std::string aProgramName, report_time aReport) { export_factory(aChart, std::string(aFilename), aProgramName, aReport); }
+    void export_factory(const Chart& aChart, std::string aFilename, std::string aProgramName, report_time aReport = report_time::no);
+    inline void export_factory(const Chart& aChart, std::string_view aFilename, std::string aProgramName, report_time aReport = report_time::no) { export_factory(aChart, std::string(aFilename), aProgramName, aReport); }
 
     enum class export_format { ace, save };
-    std::string export_factory(const Chart& aChart, export_format aFormat, std::string aProgramName, report_time aReport);
+    std::string export_factory(const Chart& aChart, export_format aFormat, std::string aProgramName, report_time aReport = report_time::no);
 
 } // namespace acmacs::chart
 
