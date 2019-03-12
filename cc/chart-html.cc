22bc4e947c33dbe7acc597d3e746e7bcb8f2a797
Eugene Skepner <git@skepner.eu>
2019-03-12 14:02:25 +0100

    chart-html: grouping sera

--- cc/chart-html.cc
+++ cc/chart-html.cc
@@ -14,7 +14,7 @@
 using group_t = std::vector<size_t>;
 using groups_t = std::vector<std::pair<std::string, group_t>>;
 
-static void contents(std::ostream& output, const acmacs::chart::Chart& chart, const groups_t& groups, const group_t& sera_to_show, bool all_fields, bool rest_group, bool only_with_titers);
+static void contents(std::ostream& output, const acmacs::chart::Chart& chart, const groups_t& groups, const groups_t& serum_groups, bool all_fields, bool rest_group, bool only_with_titers);
 static void header(std::ostream& output, std::string table_name);
 static void footer(std::ostream& output);
 static std::string html_escape(std::string source);
@@ -28,6 +28,8 @@ struct Options : public argv
 
     option<str_array> groups{*this, "group", desc{"comma separated indexes of antigens in the group"}};
     option<str_array> group_names{*this, "group-name"};
+    option<str_array> serum_groups{*this, "serum-group", desc{"comma separated indexes of sera in the group"}};
+    option<str_array> serum_group_names{*this, "serum-group-name"};
     option<str>       sera{*this, "sera", desc{"just show the specified sera (comma separated indexes) in the table"}};
     option<bool>      no_rest_group{*this, "no-rest-group", desc{"do not show bottom group with antigens not found in any of the specified groups"}};
     option<bool>      only_with_titers{*this, "only-with-titers", desc{"do not show antigens that have no titers against listed sera"}};
@@ -57,14 +59,16 @@ int main(int argc, char* const argv[])
             groups.emplace_back(group_name, acmacs::string::split_into_uint(opt.groups->at(group_no), ","));
         }
 
-        group_t sera_to_show;
-        if (opt.sera.has_value())
-            sera_to_show = acmacs::string::split_into_uint(*opt.sera, ",");
-        else
-            acmacs::fill_with_indexes(sera_to_show, chart->number_of_sera());
+        groups_t serum_groups;
+        for (size_t group_no = 0; group_no < opt.serum_groups->size(); ++group_no) {
+            const std::string serum_group_name = group_no < opt.serum_group_names->size() ? static_cast<std::string>(opt.serum_group_names->at(group_no)) : std::string{};
+            serum_groups.emplace_back(serum_group_name, acmacs::string::split_into_uint(opt.serum_groups->at(group_no), ","));
+        }
+        if (opt.sera.has_value() && serum_groups.empty())
+            serum_groups.emplace_back("", acmacs::string::split_into_uint(*opt.sera, ","));
 
         header(output, chart->make_name());
-        contents(output, *chart, groups, sera_to_show, opt.all_fields, !opt.no_rest_group, opt.only_with_titers);
+        contents(output, *chart, groups, serum_groups, opt.all_fields, !opt.no_rest_group, opt.only_with_titers);
         footer(output);
         output.close();
 
@@ -92,7 +96,7 @@ struct AntigenFields
     size_t skip_right() const { return size_t(dates) + size_t(lab_ids); }
 };
 
-static void serum_rows(std::ostream& output, const acmacs::chart::Chart& chart, const group_t& sera_to_show, bool all_fields, const AntigenFields& antigen_fields, const acmacs::chart::PointIndexList& having_too_few_numeric_titers);
+static std::vector<size_t> serum_rows(std::ostream& output, const acmacs::chart::Chart& chart, const groups_t& serum_groups, bool all_fields, const AntigenFields& antigen_fields, const acmacs::chart::PointIndexList& having_too_few_numeric_titers, bool rest_group);
 
 inline bool field_present(const std::vector<std::string>& src)
 {
@@ -120,12 +124,15 @@ inline const char* make_titer_class(const acmacs::chart::Titer& titer)
 
 // ----------------------------------------------------------------------
 
-void contents(std::ostream& output, const acmacs::chart::Chart& chart, const groups_t& groups, const group_t& sera_to_show, bool all_fields, bool rest_group, bool only_with_titers)
+void contents(std::ostream& output, const acmacs::chart::Chart& chart, const groups_t& groups, const groups_t& serum_groups, bool all_fields, bool rest_group, bool only_with_titers)
 {
     output << "<h3>" << chart.make_name() << "</h3>\n";
     auto antigens = chart.antigens();
     auto sera = chart.sera();
+    const auto number_of_sera = sera->size();
     const auto indexes_having_too_few_numeric_titers = chart.titers()->having_too_few_numeric_titers();
+    const auto number_of_sera_to_show =
+        rest_group ? number_of_sera : std::accumulate(std::begin(serum_groups), std::end(serum_groups), 0UL, [](size_t sum, const auto& group) { return sum + group.second.size(); });
 
     std::vector<std::string> annotations(antigens->size()), reassortants(antigens->size()), passages(antigens->size()), dates(antigens->size()), lab_ids(antigens->size());
     for (auto [ag_no, antigen] : acmacs::enumerate(*antigens)) {
@@ -140,7 +147,7 @@ void contents(std::ostream& output, const acmacs::chart::Chart& chart, const gro
     const AntigenFields antigen_fields{field_present(annotations), field_present(reassortants), field_present(passages), field_present(dates), field_present(lab_ids)};
 
     output << "<table>\n";
-    serum_rows(output, chart, sera_to_show, all_fields, antigen_fields, indexes_having_too_few_numeric_titers);
+    const auto sera_rest = serum_rows(output, chart, serum_groups, all_fields, antigen_fields, indexes_having_too_few_numeric_titers, rest_group);
 
     auto titers = chart.titers();
 
@@ -173,11 +180,21 @@ void contents(std::ostream& output, const acmacs::chart::Chart& chart, const gro
         }
 
         // titers
-        for (auto [index, sr_no] : acmacs::enumerate(sera_to_show)) {
-            const char* titer_class = make_titer_class(titers_of_antigen[index]);
-            const char* titer_pos_class = sr_no == sera_to_show.front() ? "titer-pos-left" : (sr_no == sera_to_show.back() ? "titer-pos-right" : "titer-pos-middle");
-            const auto sr_no_class = "sr-" + std::to_string(sr_no);
-            output << "<td class=\"titer " << titer_class << ' ' << titer_pos_class << ' ' << sr_no_class << "\">" << html_escape(titers_of_antigen[index]) << "</td>";
+        for (const auto& group : serum_groups) {
+            bool group_begin = true;
+            for (auto sr_no : group.second) {
+                output << "<td class=\"titer titer-pos-middle sr-" << sr_no << ' ' << make_titer_class(titers_of_antigen[sr_no]) << (group_begin ? " sr-group-begin" : "") << "\">"
+                       << html_escape(titers_of_antigen[sr_no]) << "</td>";
+                group_begin = false;
+            }
+        }
+        if (!sera_rest.empty()) {
+            bool group_begin = true;
+            for (auto sr_no : sera_rest) {
+                output << "<td class=\"titer titer-pos-middle sr-" << sr_no << ' ' << make_titer_class(titers_of_antigen[sr_no]) << (group_begin ? " sr-group-begin" : "") << "\">"
+                       << html_escape(titers_of_antigen[sr_no]) << "</td>";
+                group_begin = false;
+            }
         }
 
         if (antigen_fields.dates)
@@ -188,7 +205,7 @@ void contents(std::ostream& output, const acmacs::chart::Chart& chart, const gro
     };
 
     const auto make_group_sepeartor = [&]() {
-        output << "<tr class=\"group-separator\"><td colspan=" << (antigen_fields.skip_left() + 2 + sera_to_show.size() + antigen_fields.skip_right()) << ">A</td></tr>";
+        output << "<tr class=\"group-separator\"><td colspan=" << (antigen_fields.skip_left() + 2 + number_of_sera_to_show + antigen_fields.skip_right()) << ">A</td></tr>";
     };
 
     std::vector<bool> antigens_written(antigens->size());
@@ -199,8 +216,8 @@ void contents(std::ostream& output, const acmacs::chart::Chart& chart, const gro
         if (!first_group)
             make_group_sepeartor();
         for (auto ag_no : group.second) {
-            std::vector<acmacs::chart::Titer> titers_of_antigen(sera_to_show.size());
-            std::transform(std::begin(sera_to_show), std::end(sera_to_show), titers_of_antigen.begin(), [ag_no, &titers](size_t sr_no) { return titers->titer(ag_no, sr_no); });
+            std::vector<acmacs::chart::Titer> titers_of_antigen(number_of_sera);
+            std::transform(acmacs::index_iterator(0UL), acmacs::index_iterator(number_of_sera), titers_of_antigen.begin(), [ag_no, &titers](size_t sr_no) { return titers->titer(ag_no, sr_no); });
             if (!only_with_titers || std::any_of(std::begin(titers_of_antigen), std::end(titers_of_antigen), [](const auto& titer) { return !titer.is_dont_care(); }))
                 make_antigen(ag_no, group.first, titers_of_antigen);
             antigens_written[ag_no] = true;
@@ -211,8 +228,8 @@ void contents(std::ostream& output, const acmacs::chart::Chart& chart, const gro
         bool rest_antigens = false;
         for (size_t ag_no = 0; ag_no < antigens_written.size(); ++ag_no) {
             if (!antigens_written[ag_no]) {
-                std::vector<acmacs::chart::Titer> titers_of_antigen(sera_to_show.size());
-                std::transform(std::begin(sera_to_show), std::end(sera_to_show), titers_of_antigen.begin(), [ag_no, &titers](size_t sr_no) { return titers->titer(ag_no, sr_no); });
+                std::vector<acmacs::chart::Titer> titers_of_antigen(number_of_sera);
+                std::transform(acmacs::index_iterator(0UL), acmacs::index_iterator(number_of_sera), titers_of_antigen.begin(), [ag_no, &titers](size_t sr_no) { return titers->titer(ag_no, sr_no); });
                 if (!only_with_titers || std::any_of(std::begin(titers_of_antigen), std::end(titers_of_antigen), [](const auto& titer) { return !titer.is_dont_care(); })) {
                     if (!rest_antigens && !first_group)
                         make_group_sepeartor();
@@ -239,65 +256,106 @@ void contents(std::ostream& output, const acmacs::chart::Chart& chart, const gro
 
 // ----------------------------------------------------------------------
 
-void serum_rows(std::ostream& output, const acmacs::chart::Chart& chart, const group_t& sera_to_show, bool all_fields, const AntigenFields& antigen_fields, const acmacs::chart::PointIndexList& having_too_few_numeric_titers)
+std::vector<size_t> serum_rows(std::ostream& output, const acmacs::chart::Chart& chart, const groups_t& serum_groups, bool all_fields, const AntigenFields& antigen_fields,
+                const acmacs::chart::PointIndexList& having_too_few_numeric_titers, bool rest_group)
 {
     auto sera = chart.sera();
+    const auto number_of_sera = sera->size();
     const auto number_of_antigens = chart.number_of_antigens();
-    auto make_skip = [&output] (size_t count) { output << "<td colspan=\"" + std::to_string(count) + "\"></td>"; };
+    auto make_skip = [&output](size_t count) { output << "<td colspan=\"" + std::to_string(count) + "\"></td>"; };
 
-      // serum no
+    // groups
     output << "<tr>\n";
     make_skip(antigen_fields.skip_left() + 2);
-    for (auto sr_no : sera_to_show)
-        output << "<td class=\"sr-no " << ("sr-" + std::to_string(sr_no)) << "\">" << (sr_no + 1) << "</td>";
+    size_t sera_in_groups = 0;
+    for (const auto& group : serum_groups) {
+        output << "<td class=\"sr-group sr-group-begin\" colspan=\"" << group.second.size() << "\">" << group.first << "</td>";
+        sera_in_groups += group.second.size();
+    }
+    if (rest_group && sera_in_groups < number_of_sera)
+        output << "<td class=\"sr-group sr-group-begin\" colspan=\"" << (number_of_sera - sera_in_groups) << "\"></td>";
     make_skip(antigen_fields.skip_right());
     output << "</tr>\n";
 
-    if (all_fields) {
-          // serum names
+    std::vector<bool> sera_written(number_of_sera);
+
+    // serum no
+    output << "<tr>\n";
+    make_skip(antigen_fields.skip_left() + 2);
+    for (const auto& group : serum_groups) {
+        bool group_begin = true;
+        for (auto sr_no : group.second) {
+            output << "<td class=\"sr-no " << ("sr-" + std::to_string(sr_no)) << (group_begin ? " sr-group-begin" : "") << "\">" << (sr_no + 1) << "</td>";
+            sera_written[sr_no] = true;
+            group_begin = false;
+        }
+    }
+    std::vector<size_t> sera_rest;
+    if (rest_group && sera_in_groups < number_of_sera) {
+        bool group_begin = true;
+        for (size_t sr_no = 0; sr_no < number_of_sera; ++sr_no) {
+            if (!sera_written[sr_no]) {
+                output << "<td class=\"sr-no " << ("sr-" + std::to_string(sr_no)) << (group_begin ? " sr-group-begin" : "") << "\">" << (sr_no + 1) << "</td>";
+                sera_rest.push_back(sr_no);
+                group_begin = false;
+            }
+        }
+    }
+    make_skip(antigen_fields.skip_right());
+    output << "</tr>\n";
+
+    auto show_row = [&output, &make_skip, &antigen_fields, &serum_groups, &sera_rest, &sera](auto show_func) {
         output << "<tr>\n";
         make_skip(antigen_fields.skip_left() + 2);
-        for (auto sr_no : sera_to_show) {
-            auto serum = sera->at(sr_no);
+        for (const auto& group : serum_groups) {
+            bool group_begin = true;
+            for (auto sr_no : group.second) {
+                show_func(sr_no, *sera->at(sr_no), group_begin);
+                group_begin = false;
+            }
+        }
+        if (!sera_rest.empty()) {
+            bool group_begin = true;
+            for (auto sr_no : sera_rest) {
+                show_func(sr_no, *sera->at(sr_no), group_begin);
+                group_begin = false;
+            }
+        }
+        make_skip(antigen_fields.skip_right());
+        output << "</tr>\n";
+    };
+
+    if (all_fields) {
+        const auto show_serum_name = [&having_too_few_numeric_titers, number_of_antigens, &output](size_t sr_no, const auto& serum, bool group_begin) {
             const char* has_too_few_numeric_titers_class = having_too_few_numeric_titers.contains(sr_no + number_of_antigens) ? " too-few-numeric-titers" : "";
-            output << "<td class=\"sr-name sr-" << sr_no << " passage-" << serum->passage().passage_type() << has_too_few_numeric_titers_class
-                   << "\">" << html_escape(serum->name_abbreviated()) << "</td>";
-        }
-        make_skip(antigen_fields.skip_right());
-        output << "</tr>\n";
+            output << "<td class=\"sr-name sr-" << sr_no << " passage-" << serum.passage().passage_type() << has_too_few_numeric_titers_class << (group_begin ? " sr-group-begin" : "") << "\">"
+                   << html_escape(serum.name_abbreviated()) << "</td>";
+        };
 
-          // serum annotations (e.g. CONC)
-        std::vector<std::string> serum_annotations(sera->size());
-        for (auto sr_no : sera_to_show) {
-            auto serum = sera->at(sr_no);
-            serum_annotations[sr_no] = string::join(" ", serum->annotations());
-        }
+        show_row(show_serum_name);
+
+        // serum annotations (e.g. CONC)
+        std::vector<std::string> serum_annotations(number_of_sera);
+        for (size_t sr_no = 0; sr_no < number_of_sera; ++sr_no)
+            serum_annotations[sr_no] = string::join(" ", sera->at(sr_no)->annotations());
         if (field_present(serum_annotations)) {
-            output << "<tr>";
-            make_skip(antigen_fields.skip_left() + 2);
-            for (auto sr_no : sera_to_show)
-                output << "<td class=\"sr-annotations " << ("sr-" + std::to_string(sr_no)) << "\">" << html_escape(serum_annotations[sr_no]) << "</td>";
-            make_skip(antigen_fields.skip_right());
-            output << "</tr>\n";
+            auto show_annotations = [&output, &serum_annotations](size_t sr_no, const auto& /*serum*/, bool group_begin) {
+                output << "<td class=\"sr-annotations " << ("sr-" + std::to_string(sr_no)) << (group_begin ? " sr-group-begin" : "") << "\">" << html_escape(serum_annotations[sr_no]) << "</td>";
+            };
+
+            show_row(show_annotations);
         }
 
-          // serum_ids
-        output << "<tr>";
-        make_skip(antigen_fields.skip_left() + 2);
-        for (auto sr_no : sera_to_show) {
-            auto serum = sera->at(sr_no);
-            const auto sr_no_class = "sr-" + std::to_string(sr_no);
-            output << "<td class=\"sr-id " << sr_no_class << "\">" << html_escape(serum->serum_id()) << "</td>";
-        }
-        make_skip(antigen_fields.skip_right());
-        output << "</tr>\n";
+        // serum_ids
+        const auto show_serum_ids = [&output](size_t sr_no, const auto& serum, bool group_begin) {
+            output << "<td class=\"sr-id " << "sr-" << sr_no << (group_begin ? " sr-group-begin" : "") << "\">" << html_escape(serum.serum_id()) << "</td>";
+        };
+
+        show_row(show_serum_ids);
     }
     else {
-        output << "<tr>\n";
-        make_skip(antigen_fields.skip_left() + 2);
-        for (auto sr_no : sera_to_show) {
-            auto serum = sera->at(sr_no);
-            std::string name = serum->name_abbreviated();
+        auto show_serum = [&having_too_few_numeric_titers, number_of_antigens, &output](size_t sr_no, const auto& serum, bool group_begin) {
+            std::string name = serum.name_abbreviated();
             if (name.size() > 10) {
                 std::vector<std::string> fields;
                 for (auto field : acmacs::string::split(name, "/", acmacs::string::Split::RemoveEmpty)) {
@@ -309,13 +367,14 @@ void serum_rows(std::ostream& output, const acmacs::chart::Chart& chart, const g
                 name = string::join("/", fields);
             }
             const char* has_too_few_numeric_titers_class = having_too_few_numeric_titers.contains(sr_no + number_of_antigens) ? " too-few-numeric-titers" : "";
-            output << "<td class=\"sr-name sr-" << sr_no << " passage-" << serum->passage().passage_type() << has_too_few_numeric_titers_class
-                   << "\"><div class=\"tooltip\">" << html_escape(name)
-                   << "<span class=\"tooltiptext\">" << html_escape(serum->full_name_with_passage()) << "</span></div></td>";
-        }
-        make_skip(antigen_fields.skip_right());
-        output << "</tr>\n";
+            output << "<td class=\"sr-name sr-" << sr_no << " passage-" << serum.passage().passage_type() << has_too_few_numeric_titers_class << (group_begin ? " sr-group-begin" : "")
+                   << "\"><div class=\"tooltip\">" << html_escape(name) << "<span class=\"tooltiptext\">" << html_escape(serum.full_name_with_passage()) << "</span></div></td>";
+        };
+
+        show_row(show_serum);
     }
+
+    return sera_rest;
 
 } // sera
 
@@ -330,6 +389,8 @@ void header(std::ostream& output, std::string table_name)
 <style>
 table { border-collapse: collapse; border: 1px solid grey; border-spacing: 0; }
 table.td { border: 1px solid grey; }
+.sr-group-begin { border-left: 1px solid black; }
+td.sr-group { padding: 0 0.5em; text-align: center; }
 .sr-no { text-align: center; font-size: 0.8em; }
 .sr-name { text-align: center; padding: 0 0.5em; white-space: nowrap; font-weight: bold; font-size: 1em; }
 .sr-name .tooltip .tooltiptext { top: -3em; left: -10em; }
