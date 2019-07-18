e2619efdcf300cb62e1efc8788f391fe5c1bebc0
Eugene Skepner <github@skepner.eu>
2019-07-18 14:57:44 +0200

    using argv

--- cc/chart-modify-plot-spec.cc
+++ cc/chart-modify-plot-spec.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <algorithm>
 
-#include "acmacs-base/argc-argv.hh"
+#include "acmacs-base/argv.hh"
 #include "acmacs-base/filesystem.hh"
 #include "acmacs-base/string-split.hh"
 #include "acmacs-chart-2/factory-import.hh"
@@ -19,66 +19,63 @@ inline void extend(std::vector<size_t>& target, std::vector<size_t>&& source, si
     });
 }
 
+using namespace acmacs::argv;
+struct Options : public argv
+{
+    Options(int a_argc, const char* const a_argv[], on_error on_err = on_error::exit) : argv() { parse(a_argc, a_argv, on_err); }
+
+    option<str> points{*this, arg_name{"points"}, desc{"comma separated list of point indexes, no spaces!"}};
+    option<str> antigens{*this, arg_name{"antigens"}, desc{"comma separated list of antigen indexes, no spaces!"}};
+    option<str> sera{*this, arg_name{"sera"}, desc{"comma separated list of serum indexes, no spaces!"}};
+    option<double> size{*this, arg_name{"size"}, desc{"change point size"}};
+    option<str> fill{*this, arg_name{"fill"}, desc{"change point fill color"}};
+    option<str> outline{*this, arg_name{"outline"}, desc{"change point outline color"}};
+
+    argument<str> input_chart{*this, arg_name{"input-chart"}, mandatory};
+    argument<str> output_chart{*this, arg_name{"output-chart"}, mandatory};
+};
+
 int main(int argc, char* const argv[])
 {
     int exit_code = 0;
     try {
-        argc_argv args(argc, argv, {
-                {"--points", "", "comma separated list of point indexes, no spaces!"},
-                {"--antigens", "", "comma separated list of antigen indexes, no spaces!"},
-                {"--sera", "", "comma separated list of serum indexes, no spaces!"},
+        Options opt(argc, argv);
 
-                {"--size", 0.0, "change point size"},
-                {"--fill", "", "change point fill color"},
-                {"--outline", "", "change point outline color"},
+        auto chart = acmacs::chart::import_from_file(opt.input_chart);
+        std::cout << chart->make_info() << '\n';
 
-                {"--time", false, "report time of loading chart"},
-                {"--verbose", false},
-                {"-h", false},
-                {"--help", false},
-                {"-v", false},
-        });
-        if (args["-h"] || args["--help"] || args.number_of_arguments() != 2) {
-            std::cerr << "Usage: " << args.program() << " [options] <input-chart-file> <output-chart-file>\n" << args.usage_options() << '\n';
-            exit_code = 1;
+        std::vector<size_t> points;
+        if (opt.points)
+            extend(points, acmacs::string::split_into_size_t(static_cast<std::string_view>(opt.points), ","), 0, chart->number_of_points());
+        if (opt.antigens)
+            extend(points, acmacs::string::split_into_size_t(static_cast<std::string_view>(opt.antigens), ","), 0, chart->number_of_antigens());
+        if (opt.sera)
+            extend(points, acmacs::string::split_into_size_t(static_cast<std::string_view>(opt.sera), ","), chart->number_of_antigens(), chart->number_of_sera());
+        std::sort(points.begin(), points.end());
+        points.erase(std::unique(points.begin(), points.end()), points.end());
+        if (points.empty())
+            throw std::runtime_error("No point indexes were provided, use --points, --antigens, --sera");
+        // else if (!points.empty() && points.back() >= chart->number_of_points())
+        //     throw std::runtime_error("Invalid point index: " + acmacs::to_string(points.back()) + ", expected in range 0.." + acmacs::to_string(chart->number_of_points() - 1) + " inclusive");
+        std::cerr << "points: " << points << '\n';
+
+        acmacs::chart::ChartModify chart_modify(chart);
+        auto plot_spec = chart_modify.plot_spec_modify();
+
+        if (opt.size) {
+            for (auto point_no : points)
+                plot_spec->size(point_no, Pixels{opt.size});
         }
-        else {
-            auto chart = acmacs::chart::import_from_file(args[0], acmacs::chart::Verify::None, do_report_time(args["--time"]));
-            std::cout << chart->make_info() << '\n';
-
-            std::vector<size_t> points;
-            if (args["--points"])
-                extend(points, acmacs::string::split_into_size_t(static_cast<std::string_view>(args["--points"]), ","), 0, chart->number_of_points());
-            if (args["--antigens"])
-                extend(points, acmacs::string::split_into_size_t(static_cast<std::string_view>(args["--antigens"]), ","), 0, chart->number_of_antigens());
-            if (args["--sera"])
-                extend(points, acmacs::string::split_into_size_t(static_cast<std::string_view>(args["--sera"]), ","), chart->number_of_antigens(), chart->number_of_sera());
-            std::sort(points.begin(), points.end());
-            points.erase(std::unique(points.begin(), points.end()), points.end());
-            if (points.empty())
-                throw std::runtime_error("No point indexes were provided, use --points, --antigens, --sera");
-            // else if (!points.empty() && points.back() >= chart->number_of_points())
-            //     throw std::runtime_error("Invalid point index: " + acmacs::to_string(points.back()) + ", expected in range 0.." + acmacs::to_string(chart->number_of_points() - 1) + " inclusive");
-            std::cerr << "points: " << points << '\n';
-
-            acmacs::chart::ChartModify chart_modify(chart);
-            auto plot_spec = chart_modify.plot_spec_modify();
-
-            if (args["--size"].present()) {
-                for (auto point_no: points)
-                    plot_spec->size(point_no, Pixels{args["--size"]});
-            }
-            if (args["--fill"].present()) {
-                for (auto point_no: points)
-                    plot_spec->fill(point_no, Color(static_cast<std::string_view>(args["--fill"])));
-            }
-            if (args["--outline"].present()) {
-                for (auto point_no: points)
-                    plot_spec->outline(point_no, Color(static_cast<std::string_view>(args["--outline"])));
-            }
-
-            acmacs::chart::export_factory(chart_modify, args[1], fs::path(args.program()).filename(), do_report_time(args["--time"]));
+        if (opt.fill) {
+            for (auto point_no : points)
+                plot_spec->fill(point_no, Color(opt.fill));
         }
+        if (opt.outline) {
+            for (auto point_no : points)
+                plot_spec->outline(point_no, Color(opt.outline));
+        }
+
+        acmacs::chart::export_factory(chart_modify, opt.output_chart, fs::path(opt.program_name()).filename());
     }
     catch (std::exception& err) {
         std::cerr << "ERROR: " << err.what() << '\n';
--- cc/chart-titer-merging-from-layers.cc
+++ cc/chart-titer-merging-from-layers.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#include "acmacs-base/argc-argv.hh"
+#include "acmacs-base/argv.hh"
 #include "acmacs-base/range.hh"
 #include "acmacs-base/stream.hh"
 #include "acmacs-chart-2/factory-import.hh"
@@ -8,47 +8,42 @@
 
 // ----------------------------------------------------------------------
 
+using namespace acmacs::argv;
+struct Options : public argv
+{
+    Options(int a_argc, const char* const a_argv[], on_error on_err = on_error::exit) : argv() { parse(a_argc, a_argv, on_err); }
+
+    argument<str> chart{*this, arg_name{"chart"}, mandatory};
+    argument<size_t> antigen_no{*this, arg_name{"antigen-no"}, mandatory};
+    argument<size_t> serum_no{*this, arg_name{"serum-no"}, mandatory};
+};
+
 int main(int argc, char* const argv[])
 {
     int exit_code = 0;
     try {
-        argc_argv args(argc, argv,
-                       {
-                           {"--time", false, "report time of loading chart"},
-                           {"--verbose", false},
-                           {"-h", false},
-                           {"--help", false},
-                           {"-v", false},
-                       });
-        if (args["-h"] || args["--help"] || args.number_of_arguments() != 3) {
-            std::cerr << "Usage: " << args.program() << " [options] <chart-file> <antigen-no> <serum-no>\n" << args.usage_options() << '\n';
-            exit_code = 1;
-        }
-        else {
-            const auto report = do_report_time(args["--time"]);
-            auto chart = acmacs::chart::import_from_file(args[0], acmacs::chart::Verify::None, report);
-            auto titers = chart->titers();
-            if (titers->number_of_layers() < 2)
-                throw std::runtime_error{"chart without layers"};
-            auto antigens = chart->antigens();
-            const size_t antigen_no = std::stoul(args[1]);
-            if (antigen_no >= antigens->size())
-                throw std::runtime_error{"antigen number out of range"};
-            auto sera = chart->sera();
-            const size_t serum_no = std::stoul(args[2]);
-            if (serum_no >= sera->size())
-                throw std::runtime_error{"serum number out of range"};
-            auto info = chart->info();
+        Options opt(argc, argv);
 
-            std::cout << "INFO: AG " << std::setw(4) << std::right << antigen_no << ' ' << antigens->at(antigen_no)->full_name() << '\n';
-            std::cout << "INFO: SR " << std::setw(4) << std::right << serum_no << ' ' << sera->at(serum_no)->full_name() << '\n';
-            std::cout << "INFO: Layers: " << titers->number_of_layers() << '\n';
-            std::cout << "INFO: Merged: " << titers->titer(antigen_no, serum_no) << '\n';
-            std::cout << "INFO: In layers:\n"; // << titers->titers_for_layers(antigen_no, serum_no) << '\n';
-            for (auto layer_no : acmacs::range(titers->number_of_layers())) {
-                if (const auto titer = titers->titer_of_layer(layer_no, antigen_no, serum_no); !titer.is_dont_care()) {
-                    std::cout << std::setw(4) << std::right << layer_no << ' ' << std::setw(10) << std::left << info->source(layer_no)->date() << titer << '\n';
-                }
+        auto chart = acmacs::chart::import_from_file(opt.chart);
+        auto titers = chart->titers();
+        if (titers->number_of_layers() < 2)
+            throw std::runtime_error{"chart without layers"};
+        auto antigens = chart->antigens();
+        if (*opt.antigen_no >= antigens->size())
+            throw std::runtime_error{"antigen number out of range"};
+        auto sera = chart->sera();
+        if (*opt.serum_no >= sera->size())
+            throw std::runtime_error{"serum number out of range"};
+        auto info = chart->info();
+
+        std::cout << "INFO: AG " << std::setw(4) << std::right << *opt.antigen_no << ' ' << antigens->at(opt.antigen_no)->full_name() << '\n';
+        std::cout << "INFO: SR " << std::setw(4) << std::right << *opt.serum_no << ' ' << sera->at(opt.serum_no)->full_name() << '\n';
+        std::cout << "INFO: Layers: " << titers->number_of_layers() << '\n';
+        std::cout << "INFO: Merged: " << titers->titer(opt.antigen_no, opt.serum_no) << '\n';
+        std::cout << "INFO: In layers:\n"; // << titers->titers_for_layers(antigen_no, serum_no) << '\n';
+        for (auto layer_no : acmacs::range(titers->number_of_layers())) {
+            if (const auto titer = titers->titer_of_layer(layer_no, opt.antigen_no, opt.serum_no); !titer.is_dont_care()) {
+                std::cout << std::setw(4) << std::right << layer_no << ' ' << std::setw(10) << std::left << info->source(layer_no)->date() << titer << '\n';
             }
         }
     }
