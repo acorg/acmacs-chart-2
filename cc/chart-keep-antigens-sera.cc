445c199b96047e8c8ea7bdf13d18eab8d6c4ce48
Eugene Skepner <github@skepner.eu>
2020-02-23 18:41:16 +0100

    chart-keep-antigens-sera --reference-and-recent

--- cc/chart-keep-antigens-sera.cc
+++ cc/chart-keep-antigens-sera.cc
@@ -1,6 +1,4 @@
-#include <iostream>
-
-#include "acmacs-base/argc-argv.hh"
+#include "acmacs-base/argv.hh"
 #include "acmacs-base/string.hh"
 #include "acmacs-base/string-split.hh"
 #include "acmacs-chart-2/factory-import.hh"
@@ -9,49 +7,64 @@
 
 // ----------------------------------------------------------------------
 
+using namespace acmacs::argv;
+struct Options : public argv
+{
+    Options(int a_argc, const char* const a_argv[], on_error on_err = on_error::exit) : argv() { parse(a_argc, a_argv, on_err); }
+
+    option<str>  antigens_to_keep{*this, 'a', "antigens", desc{"comma or space separated list of antigen indexes to keep (0-based), keep all if empty"}};
+    option<str>  sera_to_keep{*this, 's', "sera", desc{"comma or space separated list of serum indexes to keep (0-based), keep all if empty"}};
+    option<size_t> keep_recent{*this, "reference-and-recent", desc{"keep all reference and few recent antigens to have specified number of antigens in the output"}};
+
+    argument<str> source{*this, arg_name{"source-chart"}, mandatory};
+    argument<str> output{*this, arg_name{"output-chart"}, mandatory};
+};
+
 int main(int argc, char* const argv[])
 {
     int exit_code = 0;
     try {
-        argc_argv args(argc, argv, {
-                {"-a", "", "comma or space separated list of antigen indexes (zero based), keep all if empty"},
-                {"-s", "", "comma or space separated list of serum indexes (zero based), keep all if empty"},
-                {"--time", false, "report time of loading chart"},
-                {"--verbose", false},
-                {"-h", false},
-                {"--help", false},
-                {"-v", false},
-                        });
-        if (args["-h"] || args["--help"] || args.number_of_arguments() != 2 || (args["-a"].str().empty() && args["-s"].str().empty())) {
-            std::cerr << "Usage: " << args.program() << " [options] <chart-file> <output-chart-file>\n" << args.usage_options() << '\n';
-            exit_code = 1;
-        }
-        else {
-            const auto report = do_report_time(args["--time"]);
-            acmacs::chart::ChartModify chart{acmacs::chart::import_from_file(args[0], acmacs::chart::Verify::None, report)};
+        Options opt(argc, argv);
 
-            const std::string antigens_to_keep_s(args["-a"]);
-            acmacs::chart::PointIndexList antigens_to_keep{antigens_to_keep_s.empty() ? acmacs::Indexes{} : acmacs::string::split_into_size_t(antigens_to_keep_s)};
-            if (!antigens_to_keep->empty()) {
-                acmacs::ReverseSortedIndexes antigens_to_remove(chart.number_of_antigens());
-                antigens_to_remove.remove(*antigens_to_keep);
-                std::cout << "INFO: antigens_to_remove: " << antigens_to_remove.size() << ' ' << antigens_to_remove << '\n';
-                chart.remove_antigens(antigens_to_remove);
-            }
+        acmacs::chart::ChartModify chart{acmacs::chart::import_from_file(*opt.source)};
 
-            const std::string sera_to_keep_s(args["-s"]);
-            acmacs::chart::PointIndexList sera_to_keep{sera_to_keep_s.empty() ? acmacs::Indexes{} : acmacs::string::split_into_size_t(sera_to_keep_s)};
-            if (!sera_to_keep->empty()) {
-                acmacs::ReverseSortedIndexes sera_to_remove(chart.number_of_sera());
-                sera_to_remove.remove(*sera_to_keep);
-                std::cout << "INFO: sera_to_remove: " << sera_to_remove.size() << ' ' << sera_to_remove << '\n';
-                chart.remove_sera(sera_to_remove);
+        acmacs::chart::PointIndexList antigens_to_keep{opt.antigens_to_keep->empty() ? acmacs::Indexes{} : acmacs::string::split_into_size_t(*opt.antigens_to_keep)};
+        acmacs::chart::PointIndexList sera_to_keep{opt.sera_to_keep->empty() ? acmacs::Indexes{} : acmacs::string::split_into_size_t(*opt.sera_to_keep)};
+
+        if (opt.keep_recent.has_value()) {
+            std::vector<std::pair<size_t, acmacs::chart::Date>> test_antigens;
+            auto antigens = chart.antigens();
+            for (auto [ag_no, antigen] : acmacs::enumerate(*antigens)) {
+                if (antigen->reference())
+                    antigens_to_keep.push_back(ag_no);
+                else
+                    test_antigens.emplace_back(ag_no, antigen->date());
             }
-            acmacs::chart::export_factory(chart, args[1], args.program(), report);
+            std::sort(std::begin(test_antigens), std::end(test_antigens), [](const auto& ag1, const auto& ag2) { return ag1.second > ag2.second; }); // most recent first
+            for (auto ta = test_antigens.begin(); ta != test_antigens.end() && antigens_to_keep.size() < *opt.keep_recent; ++ta)
+                antigens_to_keep.push_back(ta->first);
+
+            fmt::print(stderr, "DEBUG: antigens_to_keep {} test_antigens {}\n", antigens_to_keep.size(), test_antigens.size());
         }
+
+        if (!antigens_to_keep->empty()) {
+            acmacs::ReverseSortedIndexes antigens_to_remove(chart.number_of_antigens());
+            antigens_to_remove.remove(*antigens_to_keep);
+            std::cout << "INFO: antigens_to_remove: " << antigens_to_remove.size() << ' ' << antigens_to_remove << '\n';
+            chart.remove_antigens(antigens_to_remove);
+        }
+
+        if (!sera_to_keep->empty()) {
+            acmacs::ReverseSortedIndexes sera_to_remove(chart.number_of_sera());
+            sera_to_remove.remove(*sera_to_keep);
+            std::cout << "INFO: sera_to_remove: " << sera_to_remove.size() << ' ' << sera_to_remove << '\n';
+            chart.remove_sera(sera_to_remove);
+        }
+
+        acmacs::chart::export_factory(chart, *opt.output, opt.program_name());
     }
     catch (std::exception& err) {
-        std::cerr << "ERROR: " << err.what() << '\n';
+        fmt::print(stderr, "ERROR: {}\n", err);
         exit_code = 2;
     }
     return exit_code;
