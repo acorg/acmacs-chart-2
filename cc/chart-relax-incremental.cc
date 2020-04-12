5a4df14abebfa583a50c1f554b6cf3de283f21ac
Eugene Skepner <github@skepner.eu>
2020-04-12 10:13:46 +0200

    chart-relax-incremental

--- cc/chart-relax-incremental.cc
+++ cc/chart-relax-incremental.cc
@@ -1,6 +1,4 @@
-#include <iostream>
-
-#include "acmacs-base/argc-argv.hh"
+#include "acmacs-base/argv.hh"
 #include "acmacs-base/string.hh"
 #include "acmacs-base/string-split.hh"
 #include "acmacs-base/timeit.hh"
@@ -10,50 +8,53 @@
 
 // ----------------------------------------------------------------------
 
+using namespace acmacs::argv;
+struct Options : public argv
+{
+    Options(int a_argc, const char* const a_argv[], on_error on_err = on_error::exit) : argv() { parse(a_argc, a_argv, on_err); }
+
+    option<size_t> number_of_attempts{*this, 'n', desc{"number of optimizations"}};
+    option<bool> no_disconnect_having_few_titers{*this, "no-disconnect-having-few-titers", desc{"do not disconnect points having too few numeric titers"}};
+    option<bool> unmovable_non_nan_points{*this, "unmovable_non-nan-points", desc{"keep ag/sr of primary chart frozen (unmovable)"}};
+    option<bool> remove_source_projection{*this, "remove-source-projection"};
+    option<bool> rough{*this, "rough"};
+    option<str> method{*this, "method", dflt{"cg"}, desc{"method: lbfgs, cg"}};
+    option<double> randomization_diameter_multiplier{*this, "md", dflt{2.0}, desc{"randomization diameter multiplier"}};
+    option<size_t> keep_projections{*this, "keep-projections", dflt{0ul}, desc{"number of projections to keep, 0 - keep all"}};
+    option<int> threads{*this, "threads", dflt{0}};
+    option<bool> report_time{*this, "time", desc{"report time of loading chart"}};
+
+    argument<str> chart{*this, arg_name{"chart"}, mandatory};
+    argument<str> output_chart{*this, arg_name{"output-chart"}};
+};
+
 int main(int argc, char* const argv[])
 {
     int exit_code = 0;
     try {
-        argc_argv args(argc, argv, {
-                {"-n", 1, "number of optimizations"},
-                {"--rough", false},
-                {"--method", "cg", "method: lbfgs, cg"},
-                {"--md", 2.0, "randomization diameter multiplier"},
-                {"--keep-projections", 0, "number of projections to keep, 0 - keep all"},
-                {"--no-disconnect-having-few-titers", false, "do not disconnect points having too few numeric titers"},
-                {"--threads", 0, "number of threads to use for optimization (omp): 0 - autodetect, 1 - sequential"},
-                {"--time", false, "report time of loading chart"},
-                {"--verbose", false},
-                {"-h", false},
-                {"--help", false},
-                {"-v", false},
-                        });
-        if (args["-h"] || args["--help"] || args.number_of_arguments() < 1) {
-            std::cerr << "Usage: " << args.program() << " [options] <chart-file> [<output-chart-file>]\n" << args.usage_options() << '\n';
-            exit_code = 1;
-        }
-        else {
-            const auto report = do_report_time(args["--time"]);
-            const size_t number_of_attempts = args["-n"];
-            const Timeit ti("performing " + std::to_string(number_of_attempts) + " optimizations: ", report);
-            acmacs::chart::ChartModify chart{acmacs::chart::import_from_file(args[0], acmacs::chart::Verify::None, report)};
-            const auto precision = args["--rough"] ? acmacs::chart::optimization_precision::rough : acmacs::chart::optimization_precision::fine;
-            const auto method{acmacs::chart::optimization_method_from_string(args["--method"])};
+        Options opt(argc, argv);
+        const auto report = do_report_time(opt.report_time);
+        const Timeit ti(fmt::format("performing {} optimizations: ", opt.number_of_attempts), report);
+        acmacs::chart::ChartModify chart{acmacs::chart::import_from_file(opt.chart, acmacs::chart::Verify::None, report)};
+        const auto precision = opt.rough ? acmacs::chart::optimization_precision::rough : acmacs::chart::optimization_precision::fine;
+        const auto method{acmacs::chart::optimization_method_from_string(opt.method)};
 
-            const size_t source_projection_no = 0;
-            acmacs::chart::optimization_options options(method, precision, args["--md"]);
-            options.num_threads = args["--threads"];
-            chart.relax_incremental(source_projection_no, acmacs::chart::number_of_optimizations_t{number_of_attempts}, options, args["--no-disconnect-having-few-titers"] ? acmacs::chart::disconnect_having_too_few_titers::no : acmacs::chart::disconnect_having_too_few_titers::yes);
-            auto projections = chart.projections_modify();
-            if (const size_t keep_projections = args["--keep-projections"]; keep_projections > 0 && projections->size() > keep_projections)
-                projections->keep_just(keep_projections);
-            std::cout << chart.make_info() << '\n';
-            if (args.number_of_arguments() > 1)
-                acmacs::chart::export_factory(chart, args[1], args.program(), report);
-        }
+        const size_t source_projection_no = 0;
+        acmacs::chart::optimization_options options(method, precision, opt.randomization_diameter_multiplier);
+        options.num_threads = opt.threads;
+        chart.relax_incremental(source_projection_no, acmacs::chart::number_of_optimizations_t{*opt.number_of_attempts}, options,
+                                opt.no_disconnect_having_few_titers ? acmacs::chart::disconnect_having_too_few_titers::no : acmacs::chart::disconnect_having_too_few_titers::yes,
+                                opt.remove_source_projection ? acmacs::chart::remove_source_projection::yes : acmacs::chart::remove_source_projection::no,
+                                opt.unmovable_non_nan_points ? acmacs::chart::unmovable_non_nan_points::yes : acmacs::chart::unmovable_non_nan_points::no);
+        auto projections = chart.projections_modify();
+        if (opt.keep_projections > 0 && projections->size() > opt.keep_projections)
+            projections->keep_just(opt.keep_projections);
+        fmt::print("{}\n", chart.make_info());
+        if (!opt.output_chart.empty())
+            acmacs::chart::export_factory(chart, opt.output_chart, opt.program_name(), report);
     }
     catch (std::exception& err) {
-        std::cerr << "ERROR: " << err.what() << '\n';
+        AD_ERROR("{}", err);
         exit_code = 2;
     }
     return exit_code;
