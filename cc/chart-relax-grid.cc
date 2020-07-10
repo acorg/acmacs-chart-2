44b56d5abcce503ae4cd21edfb01d1b74a44fb88
Eugene Skepner <github@skepner.eu>
2020-07-10 16:36:20 +0200

    optim-bfgs method

--- cc/chart-relax-grid.cc
+++ cc/chart-relax-grid.cc
@@ -21,7 +21,7 @@ struct Options : public argv
     option<str>    minimum_column_basis{*this, 'm', dflt{"none"}, desc{"minimum column basis"}};
     option<str>    reorient{*this, "reorient", dflt{""}, desc{"chart to re-orient resulting projections to"}};
     option<double> grid_step{*this, "step", dflt{0.1}};
-    option<str>    method{*this, "method", dflt{"cg"}, desc{"method: lbfgs, cg"}};
+    option<str>    method{*this, "method", dflt{"cg"}, desc{"method: alglib-lbfgs, alglib-cg, optim-bfgs"}};
     option<double> max_distance_multiplier{*this, "md", dflt{2.0}, desc{"randomization diameter multiplier"}};
     option<size_t> keep_projections{*this, "keep-projections", dflt{0UL}, desc{"number of projections to keep, 0 - keep all"}};
     option<bool>   keep_original_projections{*this, "keep-original-projections"};
