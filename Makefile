ae48d2deb386ad243e2b9ac4a6491f8b1ee5df20
Eugene Skepner <github@skepner.eu>
2020-08-28 17:17:51 +0200

    chart-titer-merge

--- Makefile
+++ Makefile
@@ -62,6 +62,7 @@ TARGETS = \
   $(DIST)/chart-find-chart-with-antigens \
   $(DIST)/chart-time-series-for-country \
   $(DIST)/chart-titers-compare \
+  $(DIST)/chart-titer-merge \
   $(DIST)/test-titer-iterator \
   $(DIST)/test-chart-modify \
   $(DIST)/test-chart-create-from-scratch \
---
+++ cc/chart-titer-merge.cc
@@ -0,0 +1,40 @@
+#include "acmacs-base/argv.hh"
+#include "acmacs-chart-2/chart-modify.hh"
+
+// ----------------------------------------------------------------------
+
+using namespace acmacs::argv;
+struct Options : public argv
+{
+    Options(int a_argc, const char* const a_argv[], on_error on_err = on_error::exit) : argv() { parse(a_argc, a_argv, on_err); }
+
+    std::string_view help_pre() const override { return "calculates merged titer for the ones in the command line"; }
+    argument<str_array> titers{*this, arg_name{"titer"}, mandatory};
+};
+
+int main(int argc, char* const argv[])
+{
+    int exit_code = 0;
+    try {
+        Options opt(argc, argv);
+        if (opt.titers->size() < 2)
+            throw std::runtime_error{"at least two titers required for merging"};
+        constexpr double standard_deviation_threshold = 1.0; // lispmds: average-multiples-unless-sd-gt-1-ignore-thresholded-unless-only-entries-then-min-threshold
+
+        std::vector<acmacs::chart::Titer> titers(opt.titers->size());
+        std::transform(std::begin(opt.titers), std::end(opt.titers), std::begin(titers), [](std::string_view src) { return acmacs::chart::Titer{src}; });
+        const auto [merged, report] = acmacs::chart::TitersModify::merge_titers(titers, acmacs::chart::TitersModify::more_than_thresholded::adjust_to_next, standard_deviation_threshold);
+        fmt::print("{}\n", merged);
+        AD_INFO("{} -> {}: {}", titers, merged, acmacs::chart::TitersModify::titer_merge_report_long(report));
+    }
+    catch (std::exception& err) {
+        fmt::print(stderr, "ERROR: {}\n", err);
+        exit_code = 2;
+    }
+    return exit_code;
+}
+
+// ----------------------------------------------------------------------
+/// Local Variables:
+/// eval: (if (fboundp 'eu-rename-buffer) (eu-rename-buffer))
+/// End:
