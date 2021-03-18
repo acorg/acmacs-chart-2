1442e4a91303f775940411ab8c3c6896eb030395
Eugene Skepner <github@skepner.eu>
2021-03-18 10:45:13 +0100

    names_as_text

--- cc/text-export.hh
+++ cc/text-export.hh
@@ -1,17 +1,17 @@
 #pragma once
 
-#include <string>
-#include <optional>
+#include "acmacs-chart-2/selected-antigens-sera.hh"
 
 // ----------------------------------------------------------------------
 
 namespace acmacs::chart
 {
-    class Chart;
-
     std::string export_text(const Chart& chart);
-    std::string export_table_to_text(const Chart& chart, std::optional<size_t> just_layer = std::nullopt, bool sort=false);
+    std::string export_table_to_text(const Chart& chart, std::optional<size_t> just_layer = std::nullopt, bool sort = false);
     std::string export_info_to_text(const Chart& chart);
+
+    std::string export_names_to_text(const Chart& chart, std::string_view format, const SelectedAntigens& antigens, const SelectedSera& sera);
+    inline std::string export_names_to_text(std::shared_ptr<Chart> chart, std::string_view format) { return export_names_to_text(*chart, format, SelectedAntigens{chart}, SelectedSera{chart}); }
 
 } // namespace acmacs::chart
 
