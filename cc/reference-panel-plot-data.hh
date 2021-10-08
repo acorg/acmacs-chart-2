0e95cf11ee71d9c383a0ef5cc619edc8afbeda76
Eugene Skepner <github@skepner.eu>
2021-10-08 15:33:20 +0200

    porting to clang++13

--- cc/reference-panel-plot-data.hh
+++ cc/reference-panel-plot-data.hh
@@ -20,6 +20,7 @@ namespace acmacs::chart
         struct Titers : public std::vector<Titer>
         {
             using std::vector<Titer>::vector;
+            Titers(const Titers&) = default;
             Titers& operator=(const Titers&) = default;
 
             size_t count_non_dontcare() const
