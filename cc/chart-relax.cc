a27f88f8717cd9729a1dc979cc2b295798745074
Eugene Skepner <github@skepner.eu>
2022-03-31 10:56:13 +0200

    minor fixes

--- cc/chart-relax.cc
+++ cc/chart-relax.cc
@@ -115,6 +115,3 @@ int main(int argc, char* const argv[])
 }
 
 // ----------------------------------------------------------------------
-/// Local Variables:
-/// eval: (if (fboundp 'eu-rename-buffer) (eu-rename-buffer))
-/// End:
